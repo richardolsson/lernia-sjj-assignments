12dbb89c846922b7bae8925bb426529856a3f52b
Richard Olsson <r@richardolsson.se>
2021-09-17 09:41:34 +0200

    Add validation to prevent empty todo items

--- 01-todo/main.js
+++ 01-todo/main.js
@@ -7,6 +7,15 @@ button.addEventListener('click', function() {
   // Get text from input
   const text = input.value;
 
+  // Validate input to make sure that it's not empty
+  if (text.length == 0) {
+    document.querySelector('small').innerText = 'Input must not be empty';
+    return;
+  }
+  else {
+    document.querySelector('small').innerText = '';
+  }
+
   // Create <li> element and add to DOM
   const item = document.createElement('li');
   item.innerText = text;
