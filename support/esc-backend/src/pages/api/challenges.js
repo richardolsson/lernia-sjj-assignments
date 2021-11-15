61e25a547c41f548c3b9202defc73eb9d59dfd5a
Richard Olsson <r@richardolsson.se>
2021-11-15 07:43:58 +0100

    Move some random generation functions to new random module

--- support/esc-backend/src/pages/api/challenges.js
+++ support/esc-backend/src/pages/api/challenges.js
@@ -1,4 +1,6 @@
 import { initCorsMiddleware } from "../../utils/cors";
+import { createDateSeed, initState } from "../../utils/random";
+
 
 const TITLES_FIRST = [
   "Challenge",
@@ -59,33 +61,9 @@ const LABELS = [
 
 const NUMBERS = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 
-function initState(seed, prefixes) {
-  const state = {};
-  const seedNumbers = seed
-    .split("")
-    .map((s, idx) => (idx * parseInt(s) + parseInt(s)) % 10);
-  let nextIdx = seedNumbers[seedNumbers.length - 1] % seedNumbers.length;
-
-  function getNext() {
-    const n = seedNumbers[nextIdx++];
-    nextIdx = nextIdx % seedNumbers.length;
-    return n;
-  }
-
-  for (const prefix of prefixes) {
-    state[`${prefix}Index`] = getNext();
-    state[`${prefix}Delta`] = getNext() + 1;
-  }
-
-  return state;
-}
-
 function createRandomChallenges(count) {
   const d = new Date();
-  const seedStr =
-    d.getFullYear().toString() +
-    (d.getMonth() + 1).toString().padStart(2, "0") +
-    d.getDate().toString().padStart(2, "0");
+  const seedStr = createDateSeed(d);
 
   const state = initState(seedStr, [
     "title",
