1b02106f372d7578fbd4c219248f9c92fa5cf227
Richard Olsson <r@richardolsson.se>
2021-11-15 14:06:21 +0100

    Create time-cached data retriever

--- examples/211115/oofilters/oo.js
+++ examples/211115/oofilters/oo.js
@@ -15,6 +15,29 @@ class DataRetriever {
   }
 }
 
+class TimeCachedDataRetriever {
+  constructor() {
+    this.challenges = null;
+    this.lastLoad = null;
+  }
+
+  async load() {
+    const timeSinceLastLoad = this.lastLoad?
+      (new Date() - this.lastLoad) : Number.POSITIVE_INFINITY;
+
+    if (!this.challenges || timeSinceLastLoad > 2000) {
+      const url = 'https://lernia-sjj-assignments.vercel.app/api/challenges';
+      const response = await fetch(url);
+      const data = await response.json();
+
+      this.lastLoad = new Date();
+      this.challenges = data.challenges;
+    }
+
+    return this.challenges;
+  }
+}
+
 class Challenge {
   constructor(data) {
     this.title = data.title;
