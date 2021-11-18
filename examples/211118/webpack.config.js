2b4d7f0e4a0b46012eed1f9c3db5f97d65f35663
Richard Olsson <r@richardolsson.se>
2021-11-18 12:52:37 +0100

    Set up JSX build env

--- examples/211118/webpack.config.js
+++ examples/211118/webpack.config.js
@@ -1,9 +1,21 @@
 const path = require("path");
 
 module.exports = {
-  entry: "./src/main.js",
+  entry: ["@babel/polyfill", "./src/main.js"],
   mode: "development",
   target: "web",
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ["babel-loader"],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ["*", ".js", ".jsx"],
+  },
   output: {
     path: path.resolve(__dirname),
     filename: "bundle.js",
