c09b2e5d93dc14436a0f08c7e8c14fa8125115f8
Richard Olsson <r@richardolsson.se>
2021-11-18 12:35:22 +0100

    Set up React build

--- examples/211118/webpack.config.js
+++ examples/211118/webpack.config.js
@@ -2,7 +2,7 @@ const path = require("path");
 
 module.exports = {
   entry: "./src/main.js",
-  mode: "production",
+  mode: "development",
   target: "web",
   output: {
     path: path.resolve(__dirname),
