e324d01fb74f83b39fae4092b5bb84ab4fabee8b
Richard Olsson <r@richardolsson.se>
2022-01-10 14:47:12 +0100

    Respond with 405 for non-GET method to /hello

--- examples/220110/express-example/index.js
+++ examples/220110/express-example/index.js
@@ -6,6 +6,10 @@ app.get("/hello", (request, response) => {
   response.send("Hello, world!");
 });
 
+app.use("/hello", (request, response) => {
+  response.status(405).end();
+});
+
 app.use("/goodbye", (request, response) => {
   response.send("Goodbye, world!");
 });
