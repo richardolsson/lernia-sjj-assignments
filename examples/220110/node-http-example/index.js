f03fd2aa66a8968a8b01a2210d88fa758805b173
Richard Olsson <r@richardolsson.se>
2022-01-10 14:28:44 +0100

    More complex request logic in simple node HTTP server

--- examples/220110/node-http-example/index.js
+++ examples/220110/node-http-example/index.js
@@ -4,9 +4,16 @@ const server = http.createServer((request, response) => {
   response.statusCode = 200;
 
   if (request.url == "/hello") {
-    response.write("Hello, world!");
+    if (request.method == 'GET') {
+      response.write("Hello, world!");
+    }
+    else {
+      response.statusCode = 405;
+    }
   } else if (request.url == "/goodbye") {
     response.write("Goodbye, world!");
+  } else {
+    response.statusCode = 404;
   }
 
   response.end();
