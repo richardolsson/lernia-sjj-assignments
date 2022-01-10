68c72978b3374e0d5a8816538efcfb0789b615fc
Richard Olsson <r@richardolsson.se>
2022-01-10 15:16:42 +0100

    Add express routes for / and /main.css

--- examples/220110/express-files/index.js
+++ examples/220110/express-files/index.js
@@ -1,5 +1,18 @@
 import express from "express";
+import fs from "fs/promises";
 
 const app = express();
+
+app.get('/', async (request, response) => {
+  const fileBuf = await fs.readFile('./files/index.html');
+  response.type('html');
+  response.send(fileBuf);
+});
+
+app.get('/main.css', async (request, response) => {
+  const fileBuf = await fs.readFile('./files/main.css');
+  response.type('css');
+  response.send(fileBuf);
+});
 
 app.listen(5080);
