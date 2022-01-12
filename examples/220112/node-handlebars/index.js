07c9843d054755e7d62f8382ae358ae55278c452
Richard Olsson <r@richardolsson.se>
2022-01-12 13:05:11 +0100

    Use express-handlebars to render templates

--- examples/220112/node-handlebars/index.js
+++ examples/220112/node-handlebars/index.js
@@ -1,8 +1,12 @@
+import { engine } from "express-handlebars";
 import express from "express";
 import fs from "fs/promises";
-import Handlebars from "handlebars";
+
 
 const app = express();
+app.engine("handlebars", engine());
+app.set("view engine", "handlebars");
+app.set("views", "./templates");
 
 const menu = [
   {
@@ -19,23 +23,31 @@ const menu = [
   },
 ];
 
+const menuWithActive = path => menu.map(item => {
+  return {
+    link: item.link,
+    label: item.label,
+    active: item.link == path,
+  };
+});
+
+
 app.get("/", async (req, res) => {
-  const htmlBuf = await fs.readFile("./templates/index.handlebars");
-  const htmlText = htmlBuf.toString();
-
-  const template = Handlebars.compile(htmlText);
-  const rendered = template({
-    menu: menu.map(item => {
-      return {
-        link: item.link,
-        label: item.label,
-        active: item.link == '/',
-      };
-    }),
-    path: '/',
+  res.render("index", {
+    menu: menuWithActive(req.path)
   });
+});
 
-  res.send(rendered);
+app.get("/about", async (req, res) => {
+  res.render("about", {
+    menu: menuWithActive(req.path)
+  });
+});
+
+app.get("/contact", async (req, res) => {
+  res.render("contact", {
+    menu: menuWithActive(req.path)
+  });
 });
 
 app.use("/", express.static("./static"));
