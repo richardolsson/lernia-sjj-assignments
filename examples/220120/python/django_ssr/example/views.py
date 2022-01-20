8e990ce024584f3d12a74bc83306a35d5f9e16f9
Richard Olsson <r@richardolsson.se>
2022-01-20 15:52:25 +0100

    Add dynamic menu to django SSR example

--- examples/220120/python/django_ssr/example/views.py
+++ examples/220120/python/django_ssr/example/views.py
@@ -1,10 +1,31 @@
 from django.shortcuts import render
 
+menu = [
+  {
+    'label': 'Home',
+    'link': '/',
+  },
+  {
+    'label': 'About us',
+    'link': '/about',
+  },
+  {
+    'label': 'Contact',
+    'link': '/contact',
+  },
+]
+
+def render_with_menu(request, template):
+  return render(request, template, {
+    'menu': menu,
+    'cur_page': request.path,
+  })
+
 def home(request):
-  return render(request, 'index.html')
+  return render_with_menu(request, 'index.html')
 
 def about(request):
-  return render(request, 'about.html')
+  return render_with_menu(request, 'about.html')
 
 def contact(request):
-  return render(request, 'contact.html')
+  return render_with_menu(request, 'contact.html')
