ffca61bc62e977083f5aa211b9da3c6dd452a338
Richard Olsson <r@richardolsson.se>
2022-01-20 15:37:50 +0100

    Add routes and templates for all pages

--- examples/220120/python/django_ssr/example/urls.py
+++ examples/220120/python/django_ssr/example/urls.py
@@ -3,5 +3,7 @@ from django.urls import path
 from example import views
 
 urlpatterns = [
-  path('', views.home)
+  path('', views.home),
+  path('about', views.about),
+  path('contact', views.contact),
 ]
--- examples/220120/python/django_ssr/example/views.py
+++ examples/220120/python/django_ssr/example/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 def home(request):
-  return HttpResponse('Hello, world!')
+  return render(request, 'index.html')
+
+def about(request):
+  return render(request, 'about.html')
+
+def contact(request):
+  return render(request, 'contact.html')
