83259dfb8858577a0491ec290764dabb696ea846
Richard Olsson <r@richardolsson.se>
2022-02-15 12:50:34 +0100

    Add boilerplate for kino live-coding

---
+++ 03-kino/src/app.js
@@ -0,0 +1,32 @@
+import express from "express";
+import { engine } from "express-handlebars";
+import { marked } from "marked";
+import { loadMovie, loadMovies } from "./movies.js";
+
+const app = express();
+
+app.engine("handlebars", engine({
+  helpers: {
+    markdown: md => marked(md),
+  },
+}));
+app.set("view engine", "handlebars");
+app.set("views", "./templates");
+
+app.get("/", async (req, res) => {
+  const movies = await loadMovies();
+  res.render("home", { movies });
+});
+
+app.get("/movies/:movieId", async (req, res) => {
+  const movie = await loadMovie(req.params.movieId);
+  if (movie) {
+    res.render("movie", { movie });
+  } else {
+    res.status(404).render("404");
+  }
+});
+
+app.use("/static", express.static("./static"));
+
+export default app;
