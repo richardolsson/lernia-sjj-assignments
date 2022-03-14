61d2d0c8911dc6f4a6663b279c7138f8c671e3cb
Richard Olsson <r@richardolsson.se>
2022-03-14 15:01:33 +0100

    Use state to add tasks

--- examples/220314/react-todo/src/App.js
+++ examples/220314/react-todo/src/App.js
@@ -1,11 +1,12 @@
+import { useState } from "react";
 import "./App.css";
 
 function App() {
-  const tasks = [
+  const [tasks, setTasks] = useState([
     { label: "Learn HTML" },
     { label: "Learn CSS" },
     { label: "Learn JS" },
-  ];
+  ]);
 
   const taskElements = tasks.map((task) => {
     return (
@@ -16,13 +17,22 @@ function App() {
     );
   });
 
+  const onClickOk = () => {
+    setTasks([
+      ...tasks,
+      {
+        label: "New task",
+      },
+    ]);
+  };
+
   return (
     <div className="App">
       <div className="container">
         <h1>My ToDo</h1>
         <p className="counter">0 completed</p>
         <input type="text" />
-        <button>OK</button>
+        <button onClick={onClickOk}>OK</button>
         <small className="error"></small>
         <ul>{taskElements}</ul>
       </div>
