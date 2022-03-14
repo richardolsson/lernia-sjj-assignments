4b59530c97ccf60b01d2fb6476dd073b05bb92e4
Richard Olsson <r@richardolsson.se>
2022-03-14 15:22:03 +0100

    Add code to toggle completed

--- examples/220314/react-todo/src/App.js
+++ examples/220314/react-todo/src/App.js
@@ -4,15 +4,33 @@ import "./App.css";
 function App() {
   const [text, setText] = useState("");
   const [tasks, setTasks] = useState([
-    { label: "Learn HTML" },
-    { label: "Learn CSS" },
-    { label: "Learn JS" },
+    { completed: false, label: "Learn HTML" },
+    { completed: false, label: "Learn CSS" },
+    { completed: false, label: "Learn JS" },
   ]);
 
-  const taskElements = tasks.map((task) => {
+  const taskElements = tasks.map((task, index) => {
+    const onClickTask = () => {
+      const updatedTasks = [];
+      tasks.forEach((taskToUpdate) => {
+        if (taskToUpdate === task) {
+          updatedTasks.push({
+            completed: !task.completed,
+            label: task.label,
+          });
+        } else {
+          updatedTasks.push(taskToUpdate);
+        }
+      });
+
+      setTasks(updatedTasks);
+    };
+
     return (
-      <li key={task.label}>
-        <span className="label">{task.label}</span>
+      <li key={task.label} className={task.completed ? "completed" : null}>
+        <span className="label" onClick={onClickTask}>
+          {task.label}
+        </span>
         <span className="trashcan">🗑</span>
       </li>
     );
