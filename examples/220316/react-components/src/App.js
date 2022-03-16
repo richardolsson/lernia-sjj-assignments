188ff587bbeb5fa57fc1e56df49cabcd278fcd2e
Richard Olsson <r@richardolsson.se>
2022-03-16 10:50:15 +0100

    Revert to static UI in react todo app

--- examples/220316/react-components/src/App.js
+++ examples/220316/react-components/src/App.js
@@ -1,67 +1,27 @@
-import { useState } from "react";
 import "./App.css";
 
 function App() {
-  const [text, setText] = useState("");
-  const [tasks, setTasks] = useState([
-    { completed: false, label: "Learn HTML" },
-    { completed: false, label: "Learn CSS" },
-    { completed: false, label: "Learn JS" },
-  ]);
-
-  const taskElements = tasks.map((task, index) => {
-    const onClickTask = () => {
-      setTasks([
-        ...tasks.slice(0, index),
-        {
-          ...task,
-          completed: !task.completed,
-        },
-        ...tasks.slice(index + 1),
-      ]);
-    };
-
-    const onClickTrashcan = () => {
-      setTasks(tasks.filter((taskToFilter) => taskToFilter !== task));
-    };
-
-    return (
-      <li key={task.label} className={task.completed ? "completed" : null}>
-        <span className="label" onClick={onClickTask}>
-          {task.label}
-        </span>
-        <span className="trashcan" onClick={onClickTrashcan}>
-          🗑
-        </span>
-      </li>
-    );
-  });
-
-  const onClickOk = () => {
-    setText("");
-    setTasks([
-      ...tasks,
-      {
-        label: text,
-      },
-    ]);
-  };
-
-  const onTextChange = (event) => {
-    setText(event.target.value);
-  };
-
-  const completedCount = tasks.filter((task) => task.completed).length;
-
   return (
     <div className="App">
       <div className="container">
         <h1>My ToDo</h1>
-        <p className="counter">{completedCount} completed</p>
-        <input type="text" value={text} onChange={onTextChange} />
-        <button onClick={onClickOk}>OK</button>
-        <small className="error"></small>
-        <ul>{taskElements}</ul>
+        <p className="counter">0 completed</p>
+        <input type="text" />
+        <button>OK</button>
+        <ul>
+          <li>
+            <span className="label">Learn HTML</span>
+            <span className="trashcan">🗑</span>
+          </li>
+          <li>
+            <span className="label">Learn JS</span>
+            <span className="trashcan">🗑</span>
+          </li>
+          <li>
+            <span className="label">Learn CSS</span>
+            <span className="trashcan">🗑</span>
+          </li>
+        </ul>
       </div>
     </div>
   );
