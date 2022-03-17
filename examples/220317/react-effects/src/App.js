6ff6fdc3053959cdd02a9bcaf94d64264cfe9cc4
Richard Olsson <r@richardolsson.se>
2022-03-17 16:56:27 +0100

    Use useEffect to sync tasks with local storage

--- examples/220317/react-effects/src/App.js
+++ examples/220317/react-effects/src/App.js
@@ -1,15 +1,35 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 import Count from "./Count";
 import TaskInput from "./TaskInput";
 import TaskList from "./TaskList";
 
 function App() {
-  const [tasks, setTasks] = useState([
-    { completed: false, label: "Learn HTML" },
-    { completed: false, label: "Learn CSS" },
-    { completed: false, label: "Learn JS" },
-  ]);
+  const [tasks, setTasks] = useState([]);
+
+  // Every render
+  /*
+  useEffect(() => {
+    console.log("Effect 1");
+  });
+  */
+
+  // First render only
+  useEffect(() => {
+    console.log("Loading");
+    const loadedJson = localStorage.getItem("storedTasks");
+    if (loadedJson) {
+      setTasks(JSON.parse(loadedJson));
+    } else {
+      setTasks([]);
+    }
+  }, []);
+
+  // Every render when tasks have changed
+  useEffect(() => {
+    console.log("Saving");
+    localStorage.setItem("storedTasks", JSON.stringify(tasks));
+  }, [tasks]);
 
   const onSubmit = (label) => {
     setTasks([
