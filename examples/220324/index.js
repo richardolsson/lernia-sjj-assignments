c3a98c9379e1a469d9aa540c735c750cd741b8dd
Richard Olsson <r@richardolsson.se>
2022-03-24 13:49:30 +0100

    Create slightly more complex mongoose example

--- examples/220324/index.js
+++ examples/220324/index.js
@@ -4,18 +4,16 @@ mongoose.connect("mongodb://localhost:27017/test");
 
 async function run() {
   // Create Model and Schema
-  const Cat = mongoose.model("Cat", { name: String });
+  const Room = mongoose.model("Room", {
+    size: Number,
+    inhabitant: {
+      name: String,
+      age: Number,
+    },
+  });
 
-  // Instantiate a document from Cat model
-  //const kitty = new Cat({ name: "Zildjian" });
-  //await kitty.save();
-  //console.log("Meow");
-
-  //const cats = await Cat.find();
-  //console.log(cats);
-
-  //const cat = await Cat.findOne({ name: "Fido" });
-  //console.log(cat);
+  const rooms = await Room.find({ "inhabitant.age": { $gt: 30 } });
+  console.log(rooms);
 }
 
 run();
