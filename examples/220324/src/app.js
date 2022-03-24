0f3e440b64eab398d0166150dfabe4adf084a846
Richard Olsson <r@richardolsson.se>
2022-03-24 14:02:12 +0100

    Add route to create data in database via REST API

--- examples/220324/src/app.js
+++ examples/220324/src/app.js
@@ -4,6 +4,8 @@ import { Room } from "./models.js";
 
 const app = express();
 
+app.use(express.json());
+
 // GET /rooms
 app.get("/rooms", async (req, res) => {
   const rooms = await Room.find();
@@ -11,5 +13,11 @@ app.get("/rooms", async (req, res) => {
 });
 
 // POST /rooms
+app.post("/rooms", async (req, res) => {
+  const room = new Room(req.body);
+  await room.save();
+
+  res.json(room);
+});
 
 export default app;
