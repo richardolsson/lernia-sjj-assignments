25e8ba85b7996659681184fd282bb112c8a4836d
Richard Olsson <r@richardolsson.se>
2022-03-28 07:15:47 +0200

    Log guesses in Game

--- examples/220328/cheatable-word-game/frontend/src/Game.js
+++ examples/220328/cheatable-word-game/frontend/src/Game.js
@@ -2,12 +2,27 @@ import { useState } from "react";
 
 function Game({ correctWord }) {
   const [inputText, setInputText] = useState("");
+  const [guesses, setGuesses] = useState([]);
+
+  const handleKeyUp = (keyCode) => {
+    if (keyCode === "Enter") {
+      setGuesses([...guesses, inputText]);
+      setInputText("");
+    }
+  };
+
   return (
     <div className="Game">
       <input
         value={inputText}
         onChange={(ev) => setInputText(ev.target.value)}
+        onKeyUp={(ev) => handleKeyUp(ev.code)}
       />
+      <ul>
+        {guesses.map((guess, index) => (
+          <li key={index}>{guess}</li>
+        ))}
+      </ul>
     </div>
   );
 }
