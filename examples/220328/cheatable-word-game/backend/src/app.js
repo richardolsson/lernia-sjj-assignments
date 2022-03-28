8a1fb23c37eea9400c65407daf6195cc6ce260b2
Richard Olsson <r@richardolsson.se>
2022-03-28 07:42:52 +0200

    Implement dummy API for highscores

--- examples/220328/cheatable-word-game/backend/src/app.js
+++ examples/220328/cheatable-word-game/backend/src/app.js
@@ -2,10 +2,12 @@ import cors from "cors";
 import express from "express";
 
 import { getRandomWord } from "./utils.js";
+import { loadHighscores, saveHighscore } from "./db.js";
 
 const app = express();
 
 app.use(cors());
+app.use(express.json());
 
 app.get("/api/random_word", (req, res) => {
   const word = getRandomWord();
@@ -13,6 +15,15 @@ app.get("/api/random_word", (req, res) => {
   res.json({
     word,
   });
+});
+
+app.post("/api/highscores", async (req, res) => {
+  const highscore = await saveHighscore(req.body);
+  res.status(201).json({ highscore });
+});
+
+app.get("/api/highscores", async (req, res) => {
+  res.json({ highscores: await loadHighscores() });
 });
 
 export default app;
