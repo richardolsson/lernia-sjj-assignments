04aecbef282d4c3c1944ea91d16f9e131902d1dd
Richard Olsson <r@richardolsson.se>
2022-04-11 10:50:07 +0200

    Create /api/games resource

--- 04-wordle/backend/src/app.ts
+++ 04-wordle/backend/src/app.ts
@@ -1,12 +1,41 @@
 import express from "express";
 import cors from "cors";
+import { makeWordPicker } from "./utils/wordPicker";
+import { v4 as uuidv4 } from "uuid";
 
 const app = express();
 
-app.use(cors());
+const wordPicker = makeWordPicker([
+  "HELLO",
+  "CYCLE",
+  "FALSE",
+  "TRUTH",
+  "COOL",
+  "FULL",
+  "HIGH",
+  "EMPTY",
+]);
 
-app.get("/api/random", (req, res) => {
-  res.status(200).json({ randomNumber: Math.random() });
+app.use(cors());
+app.use(express.json());
+
+const GAMES = [];
+
+app.post("/api/games", (req, res) => {
+  const wordLength: number = req.body.wordLength;
+  const unique: boolean = req.body.unique;
+
+  const game = {
+    id: uuidv4(),
+    startTime: new Date(),
+    word: wordPicker(wordLength, unique),
+    wordLength,
+    unique,
+  };
+
+  GAMES.push(game);
+
+  res.status(201).json({ id: game.id });
 });
 
 app.use(express.static("../frontend/build"));
