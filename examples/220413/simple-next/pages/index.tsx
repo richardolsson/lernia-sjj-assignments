eff17c219a72a377762c3d7df512b136fa1271b4
Richard Olsson <r@richardolsson.se>
2022-04-13 10:09:17 +0200

    Add random number render from API

--- examples/220413/simple-next/pages/index.tsx
+++ examples/220413/simple-next/pages/index.tsx
@@ -1,9 +1,22 @@
 import type { NextPage } from "next";
+import { useEffect, useState } from "react";
 
 const Home: NextPage = () => {
+  const [randomNumber, setRandomNumber] = useState(0);
+
+  useEffect(() => {
+    async function loadRandomNumber() {
+      const res = await fetch("http://localhost:3000/api/random");
+      const data = await res.json();
+      setRandomNumber(data.random);
+    }
+
+    loadRandomNumber();
+  }, []);
+
   return (
     <div>
-      <h1>Hello</h1>
+      <h1>{randomNumber}</h1>
     </div>
   );
 };
