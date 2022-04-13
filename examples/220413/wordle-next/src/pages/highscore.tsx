4fd936c176a2219d79d61a63fa6a2effca7002ac
Richard Olsson <r@richardolsson.se>
2022-04-13 11:41:09 +0200

    Create SSR highscore page

---
+++ examples/220413/wordle-next/src/pages/highscore.tsx
@@ -0,0 +1,68 @@
+import { GetServerSideProps, NextPage } from "next";
+import { GameHighscore } from "../server/models";
+
+type Highscore = {
+  name: string;
+  wordLength: number;
+  unique: boolean;
+  duration: number;
+};
+
+type HighscorePageProps = {
+  highscores: Highscore[];
+};
+
+export const getServerSideProps: GetServerSideProps<
+  HighscorePageProps
+> = async (context) => {
+  const wordLength = context.query.wordLength
+    ? parseInt(context.query.wordLength as string)
+    : undefined;
+
+  const unique = context.query.unique as "true" | "false" | undefined;
+
+  const highscores = await GameHighscore.find();
+  const filtered = highscores.filter((hs) => {
+    if (wordLength && hs.wordLength != wordLength) {
+      return false;
+    }
+
+    if (unique == "true" && !hs.unique) {
+      return false;
+    } else if (unique == "false" && hs.unique) {
+      return false;
+    }
+
+    return true;
+  });
+
+  return {
+    props: {
+      highscores: filtered.map((hs) => ({
+        name: hs.name,
+        wordLength: hs.wordLength,
+        unique: hs.unique,
+        duration: (hs.endTime.getTime() - hs.startTime.getTime()) / 1000,
+      })),
+    },
+  };
+};
+
+const HighscorePage: NextPage<HighscorePageProps> = ({ highscores }) => {
+  return (
+    <div>
+      <h1>Highscore</h1>
+      <ul>
+        {highscores.map((hs, index) => {
+          return (
+            <li key={index}>
+              {hs.name} {hs.duration}
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
+};
+
+export default HighscorePage;
