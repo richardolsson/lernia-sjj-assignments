8061f74fda348103b6536858648f8c79a2b35c92
Richard Olsson <r@richardolsson.se>
2022-05-02 13:45:03 +0200

    Create Header and Footer components

---
+++ examples/220502/indepth-react/src/components/Footer.tsx
@@ -0,0 +1,9 @@
+const Footer: React.FC = () => {
+  return (
+    <footer style={{ backgroundColor: "blue", color: "white" }}>
+      Copyright yadda yadda
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ examples/220502/indepth-react/src/components/Header.tsx
@@ -0,0 +1,9 @@
+const Header: React.FC = () => {
+  return (
+    <header style={{ backgroundColor: "gray" }}>
+      <h1>Site Name</h1>
+    </header>
+  );
+};
+
+export default Header;
