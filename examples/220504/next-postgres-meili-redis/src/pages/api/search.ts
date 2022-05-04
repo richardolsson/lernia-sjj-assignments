969b425bce0d8c9555cc4cd9db97177575bedcb4
Richard Olsson <r@richardolsson.se>
2022-05-04 11:39:45 +0200

    Add search endpoint

--- examples/220504/next-postgres-meili-redis/src/pages/api/search.ts
+++ examples/220504/next-postgres-meili-redis/src/pages/api/search.ts
@@ -1,6 +1,21 @@
+import MeiliSearch from "meilisearch";
 import { NextApiRequest, NextApiResponse } from "next";
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
-) {}
+) {
+  const msClient = new MeiliSearch({
+    host: "http://localhost:7700",
+  });
+
+  const index = msClient.index("movies");
+
+  const searchString = Array.isArray(req.query.q)
+    ? req.query.q[0]
+    : req.query.q;
+
+  const result = await index.search(searchString);
+
+  res.status(200).json(result);
+}
