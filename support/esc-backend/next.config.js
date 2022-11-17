cb759136e9bddefa5ca402518e5b9b8941ec24df
Richard Olsson <r@richardolsson.se>
2022-11-17 06:04:07 +0100

    Enable SASS and MDX

--- support/esc-backend/next.config.js
+++ support/esc-backend/next.config.js
@@ -1,3 +1,17 @@
-module.exports = {
+const withMDX = require('@next/mdx')({
+  extension: /\.mdx?$/,
+  options: {
+    // If you use remark-gfm, you'll need to use next.config.mjs
+    // as the package is ESM only
+    // https://github.com/remarkjs/remark-gfm#install
+    remarkPlugins: [],
+    rehypePlugins: [],
+    // If you use `MDXProvider`, uncomment the following line.
+    // providerImportSource: "@mdx-js/react",
+  },
+})
+
+module.exports = withMDX({
   reactStrictMode: true,
-}
+  pageExtensions: ['ts', 'tsx', 'js', 'jsx', 'md', 'mdx'],
+})
