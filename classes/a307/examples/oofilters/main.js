39703d102bda126fb90bd62de2c4bc4fd2207fd4
Richard Olsson <r@richardolsson.se>
2022-11-21 15:18:48 +0100

    Add documentation to each class

--- classes/a307/examples/oofilters/main.js
+++ classes/a307/examples/oofilters/main.js
@@ -1,3 +1,9 @@
+/**
+ * ChallengeList fulfills the role of rendering and updating the DOM
+ * for all challenges. It loads challenge data via an APIAdapter, and
+ * delegates rendering of filters and challenges to their respective
+ * classes.
+*/
 class ChallengeList {
     async render() {
         const api = new APIAdapter();
@@ -29,24 +35,23 @@ class ChallengeList {
     }
 }
 
+/**
+ * API Adapter fulfills the role of retrieving data from the API. In this
+ * example, ChallengeList uses the adapter to load challenge data.
+*/
 class APIAdapter {
     async loadChallenges() {
         const res = await fetch('https://lernia-sjj-assignments.vercel.app/api/challenges');
         const data = await res.json();
 
-        /*
-        const challenges = [];
-        for (let i=0; i<data.challenges.length; i++) {
-            challenges[i] = new Challenge(data.challenges[i]);
-        }
-
-        return challenges;
-        */
-
         return data.challenges.map(data => new Challenge(data));
     }
 }
 
+/**
+ * Challenge fulfills the role of rendering and challenge data as a DOM
+ * element (to be used in a list).
+*/
 class Challenge {
     constructor(data) {
         this.data = data;
@@ -67,6 +72,20 @@ class Challenge {
     }
 }
 
+/*********************************************************************
+ * FILTER role
+ * The filter role can be fulfilled by multiple classes (types of objects)
+ * in different ways. The role includes rendering a user interface for the
+ * filter, accepting input through that interface. It also includes a
+ * method that can be invoked to check whether a single challenge matches
+ * the filter, which is used by ChallengeList to decide which challenges
+ * to render.
+*/
+
+/**
+ * FilterCollection fulfills the filter role, by delegating messages
+ * to multiple actual filters.
+*/
 class FilterCollection {
     constructor(list) {
         this.list;
@@ -87,6 +106,11 @@ class FilterCollection {
     }
 }
 
+/**
+ * RatingFilter fulfills the filter role, by rendering a range input
+ * (slider) and checking whether challenges, when filtered, have a
+ * rating greater than the rating entered by the user via the input.
+*/
 class RatingFilter {
     constructor(list) {
         this.minRating = 0;
@@ -115,6 +139,11 @@ class RatingFilter {
     }
 }
 
+/**
+ * StringFilter fulfills the filter role, by rendering a text input
+ * and checking whether challenges, when filtered, have a title that
+ * include the text entered in the input.
+*/
 class StringFilter {
     constructor(list) {
         this.list = list;
