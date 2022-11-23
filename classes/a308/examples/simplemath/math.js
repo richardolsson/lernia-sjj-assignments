1744903d7d609fff72bc0eabf4e12e9b4907338f
Richard Olsson <r@richardolsson.se>
2022-11-23 10:55:55 +0100

    Add example of vanilla automated test

---
+++ classes/a308/examples/simplemath/math.js
@@ -0,0 +1,15 @@
+export function add(x, y) {
+    return x + y;
+}
+
+export function sub(x, y) {
+    return x - y;
+}
+
+export function mul(x, y) {
+    return x + y;
+}
+
+export function div(x, y) {
+    return x / y;
+}
