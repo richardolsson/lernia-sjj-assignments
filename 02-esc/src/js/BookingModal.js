820985859a873b7368fc50b5903b264c64c30521
Richard Olsson <r@richardolsson.se>
2022-12-08 16:28:10 +0100

    Implement crude flow through dialog

--- 02-esc/src/js/BookingModal.js
+++ 02-esc/src/js/BookingModal.js
@@ -1,4 +1,8 @@
 export default class BookingModal {
+    constructor() {
+        this.step = 1;
+    }
+
     render() {
         this.dialog = document.createElement('div');
         this.dialog.classList.add('modal');
@@ -7,7 +11,42 @@ export default class BookingModal {
     }
 
     open(challenge) {
+        this.step = 1;
+        this.challenge = challenge;
+
         this.dialog.classList.add('open');
-        this.dialog.innerText = challenge.title;
+        this.update();
+    }
+
+    update() {
+        this.dialog.innerHTML = '';
+
+        if (this.step == 1) {
+            this.dialog.innerText = 'step 1';
+            const button = document.createElement('button');
+            button.innerText = 'continue';
+            button.addEventListener('click', () => {
+                this.step = 2;
+                this.update();
+            });
+            this.dialog.append(button);
+        } else if (this.step == 2) {
+            this.dialog.innerText = 'step 2';
+            const button = document.createElement('button');
+            button.innerText = 'continue';
+            button.addEventListener('click', () => {
+                this.step = 3;
+                this.update();
+            });
+            this.dialog.append(button);
+        } else if (this.step == 3) {
+            this.dialog.innerText = 'Thank you!';
+            const button = document.createElement('button');
+            button.innerText = 'close';
+            button.addEventListener('click', () => {
+                this.dialog.classList.remove('open');
+            });
+            this.dialog.append(button);
+        }
     }
 }
