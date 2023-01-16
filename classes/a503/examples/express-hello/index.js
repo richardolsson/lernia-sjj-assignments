5c1c281e2b876d9b749c955a93f88a8ba824a631
Richard Olsson <r@richardolsson.se>
2023-01-16 13:38:17 +0100

    Handle 405 for all relevant paths in a number of different ways

--- classes/a503/examples/express-hello/index.js
+++ classes/a503/examples/express-hello/index.js
@@ -6,12 +6,28 @@ app.get('/hello', (request, response) => {
     response.send('Hello, world!');
 });
 
-app.use('/hello', (request, response) => {
-    response.status(405).end();
-});
-
 app.get('/bye', (request, response) => {
     response.send('Goodbye, world');
+});
+
+/*
+// Handle 405 cases with reused handler
+function handleUnknownMethod(req, res) {
+    res.status(405).end();
+}
+
+app.use('/bye', handleUnknownMethod);
+app.use('/hello', handleUnknownMethod);
+
+// Handle 405 cases with array of paths
+app.use(['/hello', '/bye'], (req, res) => {
+    res.status(405).end();
+});
+*/
+
+// Handle 405 cases with "regular expressions"
+app.use(/\/(hello|bye)/, (req, res) => {
+    res.status(405).end();
 });
 
 app.listen(3080);
