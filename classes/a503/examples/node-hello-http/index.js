53c947ec6142216d7a27241aee0e52f42e950a90
Richard Olsson <r@richardolsson.se>
2023-01-16 11:40:01 +0100

    Handle separate paths differently in HTTP example

--- classes/a503/examples/node-hello-http/index.js
+++ classes/a503/examples/node-hello-http/index.js
@@ -2,13 +2,20 @@ import http from "http";
 
 const server = http.createServer((request, response) => {
     if (request.method === 'GET') {
-        response.statusCode = 200;
-        response.write("Hello, world");
-        response.end();
+        if (request.url == '/hello') {
+            response.statusCode = 200;
+            response.write("Hello, world");
+        } else if (request.url == '/bye') {
+            response.statusCode = 200;
+            response.write("Goodbye, world");
+        } else {
+            response.statusCode = 404;
+        }
     } else {
         response.statusCode = 405;
-        response.end();
     }
+
+    response.end();
 });
 
 server.listen(3080);
