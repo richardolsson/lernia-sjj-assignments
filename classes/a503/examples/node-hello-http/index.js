96dbb82dc12ac63fe2af80aa77195e218b001947
Richard Olsson <r@richardolsson.se>
2023-01-16 11:30:09 +0100

    Respond with 405 for non-GET requests

--- classes/a503/examples/node-hello-http/index.js
+++ classes/a503/examples/node-hello-http/index.js
@@ -1,9 +1,14 @@
 import http from "http";
 
 const server = http.createServer((request, response) => {
-    response.statusCode = 200;
-    response.write("Hello, world");
-    response.end();
+    if (request.method === 'GET') {
+        response.statusCode = 200;
+        response.write("Hello, world");
+        response.end();
+    } else {
+        response.statusCode = 405;
+        response.end();
+    }
 });
 
 server.listen(3080);
