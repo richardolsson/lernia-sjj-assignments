1ba3ca3fc9e126caa378321fb9fb62868b6c452c
Richard Olsson <r@richardolsson.se>
2023-01-18 10:16:48 +0100

    Use POST (and body) instead of GET (querystring)

--- classes/a504/examples/ex1-node-dynamic/index.js
+++ classes/a504/examples/ex1-node-dynamic/index.js
@@ -3,13 +3,18 @@ import fs from 'fs/promises';
 
 const app = express();
 
-app.get('/', async (req, res) => {
-    const { firstName } = req.query;
+app.use(express.urlencoded());
+
+async function handleHomePage(req, res) {
+    const { firstName } = req.body;
     const buf = await fs.readFile('./static/index.html');
     const html = buf.toString().replace('$%firstName%$', firstName || 'world');
     res.type('html');
     res.send(html);
-});
+}
+
+app.get('/', handleHomePage);
+app.post('/', handleHomePage);
 
 app.use('/static', express.static('./static'));
 
