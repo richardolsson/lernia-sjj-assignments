2c683d5b1dc1c2c825227c441dfdcd1a4bf90f32
Richard Olsson <r@richardolsson.se>
2023-01-18 09:37:30 +0100

    Use query string instead of path

--- classes/a504/examples/ex1-node-dynamic/index.js
+++ classes/a504/examples/ex1-node-dynamic/index.js
@@ -4,34 +4,12 @@ import fs from 'fs/promises';
 const app = express();
 
 app.get('/', async (req, res) => {
+    const { name } = req.query;
     const buf = await fs.readFile('./static/index.html');
-    res.type('html');
-    res.send(buf);
-});
-
-app.get('/:name', async (req, res) => {
-    const name = req.params.name;
-
-    const buf = await fs.readFile('./static/index.html');
-    const html = buf.toString().replace('world', name);
-
+    const html = buf.toString().replace('world', name || 'world');
     res.type('html');
     res.send(html);
 });
-
-/*
-app.get('/static/*', async (req, res) => {
-    const path = req.path;
-    try {
-        const buf = await fs.readFile('./' + path);
-        const fields = path.split('.');
-        res.type(fields.pop());
-        res.send(buf);
-    } catch (err) {
-        res.status(404).end();
-    }
-});
-*/
 
 app.use('/static', express.static('./static'));
 
