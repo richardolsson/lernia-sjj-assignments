40b9663a58514a4a5500c4f276785602b2173f4a
Richard Olsson <r@richardolsson.se>
2023-01-18 12:03:09 +0100

    Generate menu from array

--- classes/a504/examples/ex4-express-handlebars/index.js
+++ classes/a504/examples/ex4-express-handlebars/index.js
@@ -1,21 +1,42 @@
 import { engine } from 'express-handlebars';
 import express from 'express';
 
+const MENU = [
+    {
+        label: 'Home',
+        link: '/',
+    },
+    {
+        label: 'About us',
+        link: '/about',
+    },
+    {
+        label: 'Contact us',
+        link: '/contact',
+    }
+];
+
 const app = express();
 app.engine("handlebars", engine());
 app.set("view engine", "handlebars");
 app.set("views", "./templates");
 
 app.get('/', (req, res) => {
-    res.render('index');
+    res.render('index', {
+        menu: MENU,
+    });
 });
 
 app.get('/about', (req, res) => {
-    res.render('about');
+    res.render('about', {
+        menu: MENU,
+    });
 });
 
 app.get('/contact', (req, res) => {
-    res.render('contact');
+    res.render('contact', {
+        menu: MENU,
+    });
 });
 
 app.use('/', express.static('./static'));
