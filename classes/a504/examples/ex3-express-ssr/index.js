7f1cf7500c70db6abad98f99aaa3d742eb8e590d
Richard Olsson <r@richardolsson.se>
2023-01-18 11:00:21 +0100

    Set up routes for all three pages

--- classes/a504/examples/ex3-express-ssr/index.js
+++ classes/a504/examples/ex3-express-ssr/index.js
@@ -1,9 +1,26 @@
 import express from 'express';
+import fs from 'fs/promises';
 
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send('Hello, world');
+app.get('/', async (req, res) => {
+    const templateBuf = await fs.readFile('./templates/index.html');
+    res.type('html');
+    res.send(templateBuf);
 });
+
+app.get('/about', async (req, res) => {
+    const templateBuf = await fs.readFile('./templates/about.html');
+    res.type('html');
+    res.send(templateBuf);
+});
+
+app.get('/contact', async (req, res) => {
+    const templateBuf = await fs.readFile('./templates/contact.html');
+    res.type('html');
+    res.send(templateBuf);
+});
+
+app.use('/static', express.static('./static'));
 
 app.listen(3080);
