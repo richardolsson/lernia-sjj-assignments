a0bd259089ad55a87588d0307244eaf46d9ca232
Richard Olsson <r@richardolsson.se>
2023-01-18 11:26:16 +0100

    Generate menu items from an array

--- classes/a504/examples/ex3-express-ssr/index.js
+++ classes/a504/examples/ex3-express-ssr/index.js
@@ -3,10 +3,32 @@ import fs from 'fs/promises';
 
 const app = express();
 
+const MENU = [
+    {
+        label: 'Home',
+        link: '/',
+    },
+    {
+        label: 'About us',
+        link: '/about',
+    },
+    {
+        label: 'Contact us',
+        link: '/contact',
+    }
+];
+
 async function renderTemplate(res, template) {
     const templateBuf = await fs.readFile('./templates/' + template);
+
     const headerBuf = await fs.readFile('./templates/header.html');
-    const htmlText = templateBuf.toString().replace('%header%', headerBuf.toString());
+    const htmlItems = MENU.map(itemObj => {
+        return `<li class="menu-item"><a href="${itemObj.link}">${itemObj.label}</a></li>`;
+    });
+
+    const headerText = headerBuf.toString().replace('%items%', htmlItems.join('\n'));
+
+    const htmlText = templateBuf.toString().replace('%header%', headerText);
     res.type('html');
     res.send(htmlText);
 }
