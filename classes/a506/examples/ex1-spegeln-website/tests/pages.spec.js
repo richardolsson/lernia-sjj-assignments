ea05eb4fc1eb26577b61f31e5a56cfe479fa2276
Richard Olsson <r@richardolsson.se>
2023-01-25 11:00:44 +0100

    Add test for Encanto movie page and fix bug

--- classes/a506/examples/ex1-spegeln-website/tests/pages.spec.js
+++ classes/a506/examples/ex1-spegeln-website/tests/pages.spec.js
@@ -10,3 +10,13 @@ test('home page shows list of movies', async () => {
 
   expect(response.text.includes('Shawshank')).toBeTruthy();
 });
+
+test('Encanto page returns Encanto info', async () => {
+  const response = await request(app)
+    .get('/movies/2')
+    .expect('Content-Type', 'text/html; charset=utf-8')
+    .expect(200);
+
+  expect(response.text.includes('Encanto')).toBeTruthy();
+  expect(response.text.includes('Shawshank')).toBeFalsy();
+});
