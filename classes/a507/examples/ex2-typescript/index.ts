1245aa39b400f0b07eb2b949e6c0e4636bee29c9
Richard Olsson <r@richardolsson.se>
2023-01-26 13:59:56 +0100

    Implement typescript version of code from a504

---
+++ classes/a507/examples/ex2-typescript/index.ts
@@ -0,0 +1,58 @@
+import { engine } from 'express-handlebars';
+import express from 'express';
+
+type MenuItem = {
+    label: string;
+    link: string;
+}
+
+const MENU: MenuItem[] = [
+    {
+        label: 'Home',
+        link: '/',
+    },
+    {
+        label: 'About us',
+        link: '/about',
+    },
+    {
+        label: 'Contact us',
+        link: '/contact',
+    }
+];
+
+const app = express();
+app.engine("handlebars", engine());
+app.set("view engine", "handlebars");
+app.set("views", "./templates");
+
+function menuWithActive(items: MenuItem[], path: string) {
+    return items.map(item => ({
+        active: item.link == path,
+
+        // Spread syntax
+        ...item,
+    }));
+}
+
+app.get('/', (req, res) => {
+    res.render('index', {
+        menu: menuWithActive(MENU, '/'),
+    });
+});
+
+app.get('/about', (req, res) => {
+    res.render('about', {
+        menu: menuWithActive(MENU, '/about'),
+    });
+});
+
+app.get('/contact', (req, res) => {
+    res.render('contact', {
+        menu: menuWithActive(MENU, '/contact'),
+    });
+});
+
+app.use('/', express.static('./static'));
+
+app.listen(8000);
