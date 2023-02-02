f2a8ec318d4e8390321d9262f478ee6c16fc2056
Richard Olsson <r@richardolsson.se>
2023-02-02 15:34:57 +0100

    Implement token example

--- classes/a510/examples/ex3-password-protected/index.js
+++ classes/a510/examples/ex3-password-protected/index.js
@@ -5,14 +5,30 @@ const app = express();
 
 const USERNAME = 'admin';
 const PASSWORD = 'secret';
+const TOKENS = [];
 
-app.get('/api/protected', (req, res) => {
+app.post('/api/login', (req, res) => {
   const authHeader = req.headers.authorization;
   const b64credentials = authHeader.slice(6);
   const credentials = atob(b64credentials);
   const [username, password] = credentials.split(':');
 
   if (username == USERNAME && password == PASSWORD) {
+    const randomToken = Math.random().toString();
+    TOKENS.push(randomToken);
+    res.status(200).json({
+      token: randomToken,
+    });
+  }
+});
+
+app.get('/api/protected', (req, res) => {
+  const authHeader = req.headers.authorization;
+
+  // Ex: "Bearer 0.123123123"
+  const token = authHeader.slice(7);
+
+  if (TOKENS.includes(token)) {
     res.status(200).json({ hello: 'world' });
   } else {
     res.status(401).json({error: 'not allowed' });
