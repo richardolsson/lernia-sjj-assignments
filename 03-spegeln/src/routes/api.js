8f1f853205c7be1b2bc294d7bd46d7605aadfa3d
Richard Olsson <r@richardolsson.se>
2023-02-21 15:30:06 +0100

    Delete redundent comment

--- 03-spegeln/src/routes/api.js
+++ 03-spegeln/src/routes/api.js
@@ -6,21 +6,6 @@ import NextScreeningsResource from '../resources/NextScreeningsResource.js';
 
 const router = express();
 
-/*
-Startsidan ska visa en lista på visningar de kommande dagarna. Listan ska laddas in med hjälp av webbläsarens fetch() EFTER att sidan har visats, d.v.s. inte renderas på servern.
-Endast visningar för de kommande fem dagarna ska visas
-Max 10 filmvisningar ska visas. Om det finns fler än 10 visningar de kommande fem dagarna ska bara så många dagar visas som resulterar i max 10 visningar.
-Ovanstående logik ska programmeras på servern, och testas med hjälp av ett enhetstest och mockade datakällor
-
-- [x] Create API route
-- [x] Fetch screenings from CMS API
-  - [x] Find the right URL
-  - [x] Fetch from code
-  - [x] Fetch in a way that works with tests
-- [x] Filter according to logic in assignment
-- [x] Respond with JSON
-*/
-
 router.get('/next_screenings', async (req, res) => {
   const cms = new CMSAdapter();
   const resource = new NextScreeningsResource(cms);
