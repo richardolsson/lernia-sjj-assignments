ddc3bae7985e07d3e11582757a0b6127b628dd7c
Richard Olsson <r@richardolsson.se>
2023-03-20 14:09:04 +0100

    Implement adding and rendering of todo list

--- classes/b703/examples/react-todo/src/App.js
+++ classes/b703/examples/react-todo/src/App.js
@@ -1,21 +1,38 @@
+import { useState } from 'react';
+
 import './App.css';
 
+
 function App() {
+  const [items, setItems] = useState([]);
+  const [text, setText] = useState('');
+
   return (
     <div className="container">
       <h1>My Todo</h1>
       <small>0 completed</small>
-      <form>
-        <input type="text" />
+      <form onSubmit={(ev) => {
+        setItems([...items, text]);
+        ev.preventDefault();
+      }}>
+        <input
+          type="text"
+          value={text}
+          onChange={(event) => setText(event.target.value)}
+        />
         <button type="submit">OK</button>
       </form>
       <ul className="todo-list">
-        <li className="todo-item">
-          <span>
-            Learn JS
-          </span>
-          <button className="remove-button">🗑</button>
-        </li>
+        {items.map((item, index) => {
+          return (
+            <li className="todo-item" key={index}>
+              <span>
+                {item}
+              </span>
+              <button className="remove-button">🗑</button>
+            </li>
+          );
+        })}
       </ul>
     </div>
   );
