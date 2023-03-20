b2ef8ab9129fa0b1db75462640a9a4fb4f7f93c6
Richard Olsson <r@richardolsson.se>
2023-03-20 08:57:22 +0100

    Add old examples

--- classes/b702/examples/titleCase.spec.js
+++ classes/b702/examples/titleCase.spec.js
@@ -1,6 +1,19 @@
-import { describe, it } from '@jest/globals';
+import { describe, expect, it } from '@jest/globals';
 import titleCase from './titleCase';
 
 describe('titleCase()', () => {
+  it('capitalizes all words in the sentence "All my friends"', () => {
+    const result = titleCase('all my friends');
+    expect(result).toBe('All My Friends');
+  });
 
+  it('excludes some words, e.g. "of" and "the"', () => {
+    const result = titleCase('lord of the rings');
+    expect(result).toBe('Lord of the Rings');
+  });
+
+  it('always starts with a capital letter', () => {
+    const result = titleCase('of mice and men');
+    expect(result).toBe('Of Mice and Men');
+  });
 });
