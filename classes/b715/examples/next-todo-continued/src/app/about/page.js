6d3c3c71e0a759d402cdcf071f7f726dfa823ffa
Richard Olsson <r@richardolsson.se>
2023-04-20 13:52:35 +0200

    Add logic that is only executed server-side

--- classes/b715/examples/next-todo-continued/src/app/about/page.js
+++ classes/b715/examples/next-todo-continued/src/app/about/page.js
@@ -1,5 +1,13 @@
 export default function InfoPage() {
   return (
-    <h1>INFO</h1>
+    <div>
+      <h1>INFO</h1>
+      {isSupportOpen()? <p>Call +46 701 234567</p> : null}
+    </div>
   );
+}
+
+function isSupportOpen() {
+  const now = new Date();
+  return now.getHours() > 8 && now.getHours() < 12;
 }
