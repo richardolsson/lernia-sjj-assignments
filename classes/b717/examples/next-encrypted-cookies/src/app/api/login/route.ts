e00143b66960e1d4d1f11e43fe28f738a5b7158c
Richard Olsson <r@richardolsson.se>
2023-04-26 11:22:51 +0200

    Replace username cookie with encrypted session cookie

--- classes/b717/examples/next-encrypted-cookies/src/app/api/login/route.ts
+++ classes/b717/examples/next-encrypted-cookies/src/app/api/login/route.ts
@@ -1,11 +1,20 @@
 import { NextRequest, NextResponse } from "next/server";
+import Iron from '@hapi/iron';
+import { ENC_KEY } from "@/constants";
 
 export async function POST(request: NextRequest) {
   const body = await request.json();
   const { username } = body;
 
+  const sessionData = {
+    username: username,
+    loginTime: new Date().toISOString(),
+  };
+
+  const sealed = await Iron.seal(sessionData, ENC_KEY, Iron.defaults);
+
   const response = new NextResponse();
-  response.cookies.set('username', username);
+  response.cookies.set('session', sealed);
 
   return response;
 }
--- classes/b717/examples/next-encrypted-cookies/src/app/secret/page.tsx
+++ classes/b717/examples/next-encrypted-cookies/src/app/secret/page.tsx
@@ -1,17 +1,33 @@
 import { cookies } from "next/headers";
-import { FC } from "react";
+import Iron from '@hapi/iron';
+import { ENC_KEY } from "@/constants";
 
-const SecretPage: FC = () => {
+export default async function SecretPage() {
   const allCookies = cookies();
 
-  const username = allCookies.get('username')?.value;
+  const sealed = allCookies.get('session')?.value;
+  if (!sealed) {
+    return null;
+  }
 
-  return (
-    <div>
-      {username && <h1>Welcome, {username}</h1>}
-      {!username && <h1>You are not allowed here!</h1>}
-    </div>
-  );
+  try {
+    const sessionData = await Iron.unseal(
+      sealed, ENC_KEY, Iron.defaults);
+
+    const username = sessionData.username;
+
+    return (
+      <div>
+        {username && <h1>Welcome, {username}</h1>}
+        {!username && <h1>You are not allowed here!</h1>}
+      </div>
+    );
+  } catch (err) {
+    return (
+      <div>
+        <h1>Invalid session!</h1>
+        <p>Did you tamper with your cookie?</p>
+      </div>
+    );
+  }
 }
-
-export default SecretPage;
