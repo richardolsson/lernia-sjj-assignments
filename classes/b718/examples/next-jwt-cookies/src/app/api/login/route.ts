14182219b7718c1d0c435cc9ed5ad78d1ca90d85
Richard Olsson <r@richardolsson.se>
2023-04-27 13:28:59 +0200

    Add password protection (BAD!)

--- classes/b718/examples/next-jwt-cookies/src/app/api/login/route.ts
+++ classes/b718/examples/next-jwt-cookies/src/app/api/login/route.ts
@@ -2,18 +2,29 @@ import { NextRequest, NextResponse } from "next/server";
 import JWT from 'jsonwebtoken';
 import { ENC_KEY } from "@/constants";
 
+const PASSWORDS_BY_USER: Record<string, string> = {
+  clara: 'claraspass',
+  richard: 'richardspass',
+};
+
 export async function POST(request: NextRequest) {
   const body = await request.json();
-  const { username } = body;
-
-  const sessionData = {
-    username: username,
-  };
-
-  const jwt = JWT.sign(sessionData, ENC_KEY);
+  const { username, password } = body;
 
   const response = new NextResponse();
-  response.cookies.set('session', jwt);
+
+  const correctPassword = PASSWORDS_BY_USER[username];
+  if (password == correctPassword) {
+    const sessionData = {
+      username: username,
+    };
+
+    const jwt = JWT.sign(sessionData, ENC_KEY);
+
+    response.cookies.set('session', jwt);
+  } else {
+    return new NextResponse(null, { status: 401 });
+  }
 
   return response;
 }
--- classes/b718/examples/next-jwt-cookies/src/components/LoginForm.tsx
+++ classes/b718/examples/next-jwt-cookies/src/components/LoginForm.tsx
@@ -4,6 +4,7 @@ import { FC, useState } from "react";
 
 const LoginForm: FC = () => {
   const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
 
   return (
     <div>
@@ -11,13 +12,17 @@ const LoginForm: FC = () => {
         value={username}
         onChange={ev => setUsername(ev.currentTarget.value)}
       />
+      <input
+        value={password}
+        onChange={ev => setPassword(ev.currentTarget.value)}
+      />
       <button onClick={() => {
         fetch('/api/login', {
           method: 'POST',
           headers: {
             'Content-Type': 'application/json',
           },
-          body: JSON.stringify({ username }),
+          body: JSON.stringify({ username, password }),
         });
       }}>Log in</button>
     </div>
