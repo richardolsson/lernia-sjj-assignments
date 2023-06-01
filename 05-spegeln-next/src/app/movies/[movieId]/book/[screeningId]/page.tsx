70253d04d251bdc38b7586b85594b3b7d7589478
Richard Olsson <r@richardolsson.se>
2023-06-01 16:31:42 +0200

    Create intermediate page that creates booking on server

--- 05-spegeln-next/src/app/movies/[movieId]/book/[screeningId]/page.tsx
+++ 05-spegeln-next/src/app/movies/[movieId]/book/[screeningId]/page.tsx
@@ -1,64 +1,31 @@
 'use client';
 
-import SeatMap from '@/features/booking/components/SeatMap';
-import useMovie from '@/features/movies/hooks/useMovie';
-import { Screening, SeatInfo } from '@/features/movies/types';
+import { Booking } from '@/features/booking/types';
 import apiRequest from '@/utils/apiRequest';
-import { Box, CircularProgress, Typography } from '@mui/joy';
+import { CircularProgress } from '@mui/joy';
+import { useRouter } from 'next/navigation';
 import { useEffect, useState } from 'react';
 
-type BookProps = {
+type BookingStartProps = {
   params: {
     movieId: string;
     screeningId: string;
   };
 };
 
-export default function Book({ params }: BookProps) {
+export default function BookingStart({ params }: BookingStartProps) {
+  const router = useRouter();
   const { movieId, screeningId } = params;
-  const [screening, setScreening] = useState<Screening | null>(null);
-  const [screeningLoading, setScreeningLoading] = useState(true);
-  const { movie, loading } = useMovie(parseInt(movieId));
-  const [seats, setSeats] = useState<SeatInfo[]>([]);
 
   useEffect(() => {
-    apiRequest<{ data: Screening }>(
-      'GET',
-      `https://plankton-app-xhkom.ondigitalocean.app/api/screenings/${screeningId}`
-    )
-      .then((payload) => {
-        setScreening(payload.data);
-        setScreeningLoading(false);
-      })
-      .catch(() => setScreeningLoading(false));
-
-    apiRequest<{ seats: SeatInfo[] }>(
-      'GET',
-      `/api/screenings/${screeningId}/seats`
-    ).then((payload) => {
-      setSeats(payload.seats);
+    apiRequest<Booking, {}>(
+      'POST',
+      `/api/screenings/${screeningId}/bookings`,
+      {}
+    ).then((booking) => {
+      router.push(`/movies/${movieId}/book/${screeningId}/${booking.id}`);
     });
-  }, [screeningId]);
+  }, []);
 
-  if (loading || screeningLoading) {
-    return <CircularProgress />;
-  }
-
-  if (!movie || !screening) {
-    return <Typography>Not found</Typography>;
-  }
-
-  const date = new Date(screening.attributes.start_time);
-
-  return (
-    <Box>
-      <Typography level="h2">Book: {movie.attributes.title}</Typography>
-      <Typography>
-        {date.toLocaleString()} ({screening.attributes.room})
-      </Typography>
-      <Box>
-        <SeatMap seats={seats}/>
-      </Box>
-    </Box>
-  );
+  return <CircularProgress/>;
 }
---
+++ 05-spegeln-next/src/features/booking/types.ts
@@ -0,0 +1,10 @@
+export type Booking = {
+  id: string;
+  firstName: string;
+  lastName: string;
+  email: string;
+  userId: string;
+  seatIds: string[];
+  screeningId: number;
+  status: string;
+}
