2417670a095c0c6c7322c4ccd1b49e704e997c44
Richard Olsson <r@richardolsson.se>
2023-06-01 13:31:30 +0200

    Add error message to LoginForm

--- 05-spegeln-next/src/features/account/components/LoginForm.tsx
+++ 05-spegeln-next/src/features/account/components/LoginForm.tsx
@@ -2,6 +2,7 @@
 
 import apiRequest from '@/utils/apiRequest';
 import {
+  Alert,
   Button,
   FormControl,
   FormLabel,
@@ -14,25 +15,33 @@ import { User } from '../types';
 import { LoginPostBody } from '@/app/api/login/route';
 
 type LoginFormProps = {
-  onLoginComplete: (user: User) => void;
+  onLoginComplete?: (user: User) => void;
 };
 
 const LoginForm: FC<LoginFormProps> = ({ onLoginComplete }) => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [error, setError] = useState(false);
 
   return (
     <>
       <Typography level="h2">Log in</Typography>
       <form
         onSubmit={(event) => {
+          setError(false);
           event.preventDefault();
           apiRequest<User, LoginPostBody>('POST', '/api/login', {
             email,
             password,
-          }).then((user) => {
-            onLoginComplete(user);
-          });
+          })
+            .then((user) => {
+              if (onLoginComplete) {
+                onLoginComplete(user);
+              }
+            })
+            .catch(() => {
+              setError(true);
+            });
         }}
       >
         <Grid container spacing={2}>
@@ -41,7 +50,10 @@ const LoginForm: FC<LoginFormProps> = ({ onLoginComplete }) => {
               <FormLabel>Email address</FormLabel>
               <Input
                 value={email}
-                onChange={(ev) => setEmail(ev.target.value)}
+                onChange={(ev) => {
+                  setEmail(ev.target.value);
+                  setError(false);
+                }}
               />
             </FormControl>
           </Grid>
@@ -50,11 +62,20 @@ const LoginForm: FC<LoginFormProps> = ({ onLoginComplete }) => {
               <FormLabel>Password</FormLabel>
               <Input
                 value={password}
-                onChange={(ev) => setPassword(ev.target.value)}
+                onChange={(ev) => {
+                  setPassword(ev.target.value);
+                  setError(false);
+                }}
                 type="password"
               />
             </FormControl>
           </Grid>
+          {error && (
+            <Alert color="danger" variant="soft">
+              We could not log you in. Check your email and password and try
+              again.
+            </Alert>
+          )}
           <Grid xs={12}>
             <Button type="submit">Log in</Button>
           </Grid>
