1b43264bf24a8c09329d0252f2d284e91dc30bb0
Richard Olsson <r@richardolsson.se>
2023-06-01 13:31:37 +0200

    Navigate to welcome page after logging in

--- 05-spegeln-next/src/app/login/page.tsx
+++ 05-spegeln-next/src/app/login/page.tsx
@@ -2,12 +2,18 @@
 
 import LoginForm from '@/features/account/components/LoginForm';
 import { Box, Card } from '@mui/joy';
+import { useRouter } from 'next/navigation';
 
 export default function Login() {
+  const router = useRouter();
   return (
     <Box>
       <Card sx={{ maxWidth: 600 }}>
-        <LoginForm />
+        <LoginForm
+          onLoginComplete={() => {
+            router.push('/welcome');
+          }}
+        />
       </Card>
     </Box>
   );
