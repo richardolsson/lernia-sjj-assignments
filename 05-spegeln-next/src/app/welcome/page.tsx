41573b898dfcd4594549b97d26c8205f29e79910
Richard Olsson <r@richardolsson.se>
2023-06-01 13:58:16 +0200

    Show user name if logged in

--- 05-spegeln-next/src/app/welcome/page.tsx
+++ 05-spegeln-next/src/app/welcome/page.tsx
@@ -1,7 +1,30 @@
 'use client';
 
-import { Typography } from '@mui/joy';
+import { User } from '@/features/account/types';
+import apiRequest from '@/utils/apiRequest';
+import { CircularProgress, Typography } from '@mui/joy';
+import { useEffect, useState } from 'react';
 
 export default function Welcome() {
-  return <Typography level="h2">Welcome!</Typography>;
+  const [user, setUser] = useState<User | null>(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    apiRequest<User>('GET', '/api/user')
+      .then((user) => {
+        setUser(user);
+        setLoading(false);
+      })
+      .catch(() => {
+        setLoading(false);
+      });
+  }, []);
+
+  return (
+    <Typography level="h2">
+      {user && <>Welcome, {user.firstName}!</>}
+      {!user && !loading && <>Welcome!</>}
+      {loading && <CircularProgress/>}
+    </Typography>
+  );
 }
