96d320869f566d80a460409b5fa327131b250ea3
Richard Olsson <r@richardolsson.se>
2023-06-01 14:08:26 +0200

    Hide register if already logged in

--- 05-spegeln-next/src/app/register/page.tsx
+++ 05-spegeln-next/src/app/register/page.tsx
@@ -1,11 +1,14 @@
 'use client';
 
 import RegistrationForm from '@/features/account/components/RegistrationForm';
-import { Box, Card } from '@mui/joy';
+import useLoggedInUser from '@/features/account/hooks/useLoggedInUser';
+import { Box, Card, Typography } from '@mui/joy';
 import { useRouter } from 'next/navigation';
 
 export default function Register() {
   const router = useRouter();
+  const { anon, user } = useLoggedInUser();
+
   return (
     <Box display="flex" justifyContent="space-around">
       <Card
@@ -13,11 +16,18 @@ export default function Register() {
           maxWidth: 600,
         }}
       >
-        <RegistrationForm
-          onRegistrationComplete={(user) => {
-            router.push('/welcome');
-          }}
-        />
+        {user && (
+          <Typography level="h2">
+            Silly {user.firstName}, you already have an account!
+          </Typography>
+        )}
+        {anon && (
+          <RegistrationForm
+            onRegistrationComplete={(user) => {
+              router.push('/welcome');
+            }}
+          />
+        )}
       </Card>
     </Box>
   );
