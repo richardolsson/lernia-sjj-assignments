3239dc4eab97340b147ac42a791fd607fd75ca63
Richard Olsson <r@richardolsson.se>
2023-11-13 12:55:30 +0100

    Add ESC backend

---
+++ support/esc-backend/src/pages/api/booking/available-times.js
@@ -0,0 +1,84 @@
+import { createDateSeed, initState } from "../../../utils/random";
+import { initCorsMiddleware } from "../../../utils/cors";
+
+const cors = initCorsMiddleware(["GET", "OPTIONS"]);
+
+const TIME_SLOTS = [
+  "11:00",
+  "12:30",
+  "14:00",
+  "15:30",
+  "17:00",
+  "18:30",
+  "20:00",
+];
+
+const COUNTS = [1, 3, 5, 7];
+const NUMBERS = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+function createRandomTimeSlots(seed) {
+  const state = initState(seed, [
+    "count",
+    "slot",
+  ]);
+
+  function getNext(collection, statePrefix) {
+    const idxProp = statePrefix + "Index";
+    const deltaProp = statePrefix + "Delta";
+
+    const idx = state[idxProp] % collection.length;
+    const delta = state[deltaProp];
+    const value = collection[idx];
+
+    state[idxProp] = idx + delta;
+    state[deltaProp] = NUMBERS[idx % NUMBERS.length];
+
+    return value;
+  }
+
+  const slots = [];
+  const count = getNext(COUNTS, "count");
+
+  for (let i = 0; i < count; i++) {
+    slots.push(getNext(TIME_SLOTS, "slot"));
+  }
+
+  return slots.sort();
+}
+
+export default async function handler(req, res) {
+  await cors(req, res);
+
+  if (!req.query.date) {
+    return res
+      .status(400)
+      .json({ error: "Invalid date. Use ?date=2021-05-01 format" });
+  }
+
+  if (!req.query.challenge || isNaN(parseInt(req.query.challenge))) {
+    return res
+      .status(400)
+      .json({ error: "Invalid challenge ID. Use integer passed with ?challenge=<ID>" });
+  }
+
+  const now = new Date();
+  const today = new Date(now.getFullYear(), now.getMonth(), now.getDate());
+  const nextYear = new Date(
+    now.getFullYear(),
+    now.getMonth() + 12,
+    now.getDate()
+  );
+  const date = new Date(req.query.date);
+  if (date < today || date > nextYear) {
+    return res.status(400).json({
+      error:
+        "Invalid date. Must be within next year. Use ?date=2021-05-01 format",
+    });
+  }
+
+  const seedStr = createDateSeed(date);
+  const slots = createRandomTimeSlots(seedStr);
+
+  res.setHeader("content-type", "application/json");
+  res.status(200).send(JSON.stringify({ date, slots }, null, 2));
+}
---
+++ support/esc-backend/src/pages/api/challenges.js
@@ -0,0 +1,129 @@
+import { initCorsMiddleware } from "../../utils/cors";
+import { createDateSeed, initState } from "../../utils/random";
+
+
+const TITLES_FIRST = [
+  "Challenge",
+  "Room",
+  "Hacking",
+  "Mystery",
+  "Game",
+  "Shell",
+  "Network",
+  "Project: X",
+  "Intelligence",
+  "Linux",
+  "Code",
+  "Binary dreams",
+  "Revolution",
+];
+
+const TITLES_SECOND = [
+  "of Doom",
+  "in the night",
+  "in space",
+  "demystified",
+  "for hackers",
+  "from satellite to satellite",
+  "on Windows",
+  "in the machine",
+  "online",
+  "in binary",
+  "1994",
+  "2000",
+  "3000",
+  "2.0",
+];
+
+const DESCRIPTIONS = [
+  "This room is a lot of fun",
+  "Take the ultimate challenge in this ultimate challenge",
+  "Try your hardest and succeed. Or fail",
+  "Do you have the skills to survive?",
+  "Hackers of the world, unite",
+  "Bring back the skills of the past",
+  "Go out into space and back without leaving your computer",
+  "Take over the world with code",
+];
+
+const LABELS = [
+  "hacking",
+  "ssh",
+  "web",
+  "coding",
+  "ctf",
+  "linux",
+  "bash",
+  "javascript",
+  "electronics",
+  "phreaking",
+];
+
+const NUMBERS = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+function createRandomChallenges(count) {
+  const d = new Date();
+  const seedStr = createDateSeed(d);
+
+  const state = initState(seedStr, [
+    "title",
+    "desc",
+    "type",
+    "participants",
+    "type",
+    "rating",
+    "labelCount",
+    "labelName",
+  ]);
+
+  function getNext(collection, statePrefix) {
+    const idxProp = statePrefix + "Index";
+    const deltaProp = statePrefix + "Delta";
+
+    const idx = state[idxProp] % collection.length;
+    const delta = state[deltaProp];
+    const value = collection[idx];
+
+    state[idxProp] = idx + delta;
+    state[deltaProp] = NUMBERS[idx % NUMBERS.length];
+
+    return value;
+  }
+
+  const challenges = [];
+
+  for (let i = 0; i < count; i++) {
+    const labels = [];
+    const labelCount = getNext(NUMBERS, "labelCount") / 4 + 1;
+    for (let l = 0; l < labelCount; l++) {
+      labels.push(getNext(LABELS, "labelName"));
+    }
+
+    const minParticipants = (getNext(NUMBERS, "participants") % 2) + 1;
+
+    challenges.push({
+      id: i + 1,
+      type: getNext(NUMBERS, "type") > 3 ? "onsite" : "online",
+      title:
+        getNext(TITLES_FIRST, "title") + " " + getNext(TITLES_SECOND, "title"),
+      description: getNext(DESCRIPTIONS, "desc"),
+      minParticipants: minParticipants,
+      maxParticipants: minParticipants + getNext(NUMBERS, "participants"),
+      rating: getNext(NUMBERS, "rating") / 2,
+      image: "https://placekitten.com/640/480",
+      labels: labels,
+    });
+  }
+
+  return challenges;
+}
+
+const cors = initCorsMiddleware(["GET", "OPTIONS"]);
+
+export default async function handler(req, res) {
+  await cors(req, res);
+
+  const challenges = createRandomChallenges(30);
+  res.setHeader("content-type", "application/json");
+  res.status(200).send(JSON.stringify({ challenges }, null, 2));
+}
