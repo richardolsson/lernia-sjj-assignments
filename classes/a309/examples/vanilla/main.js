dba9dde2dca7c37c45c81adfa5a250c5b33214b7
Richard Olsson <r@richardolsson.se>
2023-11-23 14:20:32 +0100

    Move counter update to event handlers

--- classes/a309/examples/vanilla/main.js
+++ classes/a309/examples/vanilla/main.js
@@ -29,8 +29,6 @@ function filterChallenges(ids) {
       item.style.display = 'none';
     }
   }
-
-  counter.textContent = `Showing ${ids.length}`;
 }
 
 function init() {
@@ -47,13 +45,16 @@ function init() {
 
   document.querySelector('#abButton').addEventListener('click', () => {
     filterChallenges(['a','b']);
+    counter.textContent = 'Showing 2';
   });
 
   document.querySelector('#bButton').addEventListener('click', () => {
     filterChallenges(['b']);
+    counter.textContent = 'Showing 1';
   });
 
   document.querySelector('#allButton').addEventListener('click', () => {
     filterChallenges(['a','b','c']);
+    counter.textContent = 'Showing 3';
   });
 }
