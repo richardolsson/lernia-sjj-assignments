58f296731bf012b9ea7068f6434c24e3603dcdcb
Richard Olsson <r@richardolsson.se>
2023-11-23 14:38:31 +0100

    Create hard-coded DOM

--- classes/a309/examples/react/src/components/App.js
+++ classes/a309/examples/react/src/components/App.js
@@ -2,6 +2,16 @@ import React from 'react';
 
 export default function App() {
   return (
-    React.createElement('h1', {}, 'Hello, world!')
+    React.createElement('div', {},
+      React.createElement('button', {}, 'Only A + B'),
+      React.createElement('button', {}, 'Only B'),
+      React.createElement('button', {}, 'All'),
+      React.createElement('p', {}, 'Showing 3'),
+      React.createElement('ul', {}, [
+        React.createElement('li', {}, 'Challenge A'),
+        React.createElement('li', {}, 'Challenge B'),
+        React.createElement('li', {}, 'Challenge C'),
+      ])
+    )
   );
 }
