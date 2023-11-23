126c720cf09a9803f9e0d7a9f8ad44604338426b
Richard Olsson <r@richardolsson.se>
2023-11-23 14:39:29 +0100

    Map elements from array

--- classes/a309/examples/react/src/components/App.js
+++ classes/a309/examples/react/src/components/App.js
@@ -1,5 +1,20 @@
 import React from 'react';
 
+const challenges = [
+  {
+    id: 'a',
+    title: 'Challenge A',
+  },
+  {
+    id: 'b',
+    title: 'Challenge B',
+  },
+  {
+    id: 'c',
+    title: 'Challenge C',
+  },
+];
+
 export default function App() {
   return (
     React.createElement('div', {},
@@ -7,11 +22,11 @@ export default function App() {
       React.createElement('button', {}, 'Only B'),
       React.createElement('button', {}, 'All'),
       React.createElement('p', {}, 'Showing 3'),
-      React.createElement('ul', {}, [
-        React.createElement('li', {}, 'Challenge A'),
-        React.createElement('li', {}, 'Challenge B'),
-        React.createElement('li', {}, 'Challenge C'),
-      ])
+      React.createElement('ul', {}, challenges.map((challenge) => {
+        return (
+          React.createElement('li', {}, challenge.title)
+        );
+      }))
     )
   );
 }
