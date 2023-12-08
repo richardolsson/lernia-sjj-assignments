dfd5f9688eba0ec9ae4b2d7a3b583aa024ea4fd6
Richard Olsson <r@richardolsson.se>
2023-12-08 16:05:31 +0100

    Use BookingModalManager on home screen

--- 02-esc/src/js/home.js
+++ 02-esc/src/js/home.js
@@ -1,26 +1,13 @@
 import FetchAPIFacade from "./api/FetchAPIFacade";
-import BookingModal from "./booking/BookingModal";
+import BookingModalManager from "./booking/BookingModalManager";
 import initializeMobileMenu from "./initializeMobileMenu";
 import Top3ChallengeList from "./lists/Top3ChallengeList";
 
 const api = new FetchAPIFacade();
 const list = new Top3ChallengeList(api);
+const modalManager = new BookingModalManager(api, list);
 
-let modal = null;
-
-list.addEventListener('bookChallenge', (event) => {
-  if (!modal) {
-    modal = new BookingModal(api, event.challenge);
-    const modalElem = modal.render();
-
-    modal.addEventListener('close', () => {
-      modalElem.remove();
-      modal = null;
-    });
-
-    document.body.append(modalElem);
-  }
-});
+modalManager.init(document.body);
 
 const listElem = await list.render();
 
