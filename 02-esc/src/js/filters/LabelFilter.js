3a91ae91f2474f146021f0ede69644fe95685e2e
Richard Olsson <r@richardolsson.se>
2023-12-08 15:54:33 +0100

    Implement UI for LabelFilter

--- 02-esc/src/js/filters/LabelFilter.js
+++ 02-esc/src/js/filters/LabelFilter.js
@@ -1,6 +1,8 @@
-export default class LabelFilter {
-  constructor(labels) {
+export default class LabelFilter extends EventTarget {
+  constructor(labels, availableLabels) {
+    super();
     this.config = { labels };
+    this.availableLabels = availableLabels;
   }
 
   getMatching(challenges) {
@@ -13,5 +15,37 @@ export default class LabelFilter {
         (requiredLabel) => challenge.labels.includes(requiredLabel)
       );
     });
+  }
+
+  render() {
+    const container = document.createElement('div');
+
+    const header = document.createElement('h2');
+    header.textContent = 'Filter by labels';
+    container.append(header);
+
+    this.availableLabels.forEach((label) => {
+      const labelElem = document.createElement('span');
+      labelElem.textContent = label;
+      labelElem.className = 'labelFilter__label'
+      container.append(labelElem);
+
+      labelElem.addEventListener('click', () => {
+        if (this.config.labels.includes(label)) {
+          labelElem.classList.remove('labelFilter__label--selected');
+          this.config.labels = this.config.labels.filter(
+            labelFromConfig => labelFromConfig != label
+          );
+        } else {
+          labelElem.classList.add('labelFilter__label--selected');
+          this.config.labels.push(label);
+        }
+
+
+        this.dispatchEvent(new Event('update'));
+      });
+    });
+
+    return container;
   }
 }
