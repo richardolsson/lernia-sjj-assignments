ebce03fa6ae24e7c822c9e3375789f3fde75e393
Richard Olsson <r@richardolsson.se>
2023-12-08 15:34:27 +0100

    Implement rendering of TypeFilter

--- 02-esc/src/js/filters/TypeFilter.js
+++ 02-esc/src/js/filters/TypeFilter.js
@@ -1,5 +1,7 @@
-export default class TypeFilter {
+export default class TypeFilter extends EventTarget {
   constructor(type) {
+    super();
+
     this.config = {
       type: type,
     };
@@ -13,5 +15,41 @@ export default class TypeFilter {
     return challenges.filter((challenge) => {
       return challenge.type == this.config.type;
     });
+  }
+
+  render() {
+    const container = document.createElement('div');
+
+    const header = document.createElement('h2');
+    header.textContent = 'Filter by type';
+    container.append(header);
+
+    const onsiteRadio = document.createElement('input');
+    onsiteRadio.type = 'radio';
+    onsiteRadio.name = 'type';
+    const onsiteLabel = document.createElement('label');
+    onsiteLabel.append(onsiteRadio);
+    onsiteLabel.append('On-site');
+    container.append(onsiteLabel);
+
+    onsiteRadio.addEventListener('change', () => {
+      this.config.type = 'onsite';
+      this.dispatchEvent(new Event('update'));
+    });
+
+    const onlineRadio = document.createElement('input');
+    onlineRadio.type = 'radio';
+    onlineRadio.name = 'type';
+    const onlineLabel = document.createElement('label');
+    onlineLabel.append(onlineRadio);
+    onlineLabel.append('Online');
+    container.append(onlineLabel);
+
+    onlineRadio.addEventListener('change', () => {
+      this.config.type = 'online';
+      this.dispatchEvent(new Event('update'));
+    });
+
+    return container;
   }
 }
