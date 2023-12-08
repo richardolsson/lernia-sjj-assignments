6a0e979431e0ad68fa5a18de77161f2b89536e47
Richard Olsson <r@richardolsson.se>
2023-12-08 15:38:14 +0100

    Implement rendering of RatingFilter

--- 02-esc/src/js/filters/RatingFilter.js
+++ 02-esc/src/js/filters/RatingFilter.js
@@ -1,5 +1,7 @@
-export default class RatingFilter {
+export default class RatingFilter extends EventTarget {
   constructor(minRating, maxRating) {
+    super();
+
     this.config = {
       minRating, maxRating,
     };
@@ -15,5 +17,39 @@ export default class RatingFilter {
 
       return maxMatches && minMatches;
     });
+  }
+
+  render() {
+    const container = document.createElement('div');
+
+    const header = document.createElement('h2');
+    header.textContent = 'Filter by rating';
+    container.append(header);
+
+    const minInput = document.createElement('input')
+    minInput.type = 'number';
+    minInput.min = 0;
+    minInput.max = 5;
+    minInput.value = 0;
+    container.append(minInput);
+
+    minInput.addEventListener('change', () => {
+      this.config.minRating = minInput.value;
+      this.dispatchEvent(new Event('update'));
+    });
+
+    const maxInput = document.createElement('input');
+    maxInput.type = 'number';
+    maxInput.min = 0;
+    maxInput.max = 5;
+    maxInput.value = 5;
+    container.append(maxInput);
+
+    maxInput.addEventListener('change', () => {
+      this.config.maxRating = maxInput.value;
+      this.dispatchEvent(new Event('update'));
+    });
+
+    return container;
   }
 }
