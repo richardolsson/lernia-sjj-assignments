92012c9661b2a37f6054f49479882625640203a6
Richard Olsson <r@richardolsson.se>
2024-01-10 11:13:21 +0100

    Add reusable "fake database" code

--- classes/a502/examples/rest-vs-rpc/io.js
+++ classes/a502/examples/rest-vs-rpc/io.js
@@ -1,3 +1,23 @@
+
+const people = [
+  { id: 1, name: 'Artemis N Folkmore', profession: 'President' },
+  { id: 2, name: 'Yev Kassim', profession: 'Restaurateur' },
+];
+
+function getPeople() {
+  return people;
+}
+
+function getPerson(personId) {
+  return people.find(person => person.id == personId);
+}
+
+function setName(personId, name) {
+  const person = getPerson(personId);
+  person.name = name;
+  return person;
+}
+
 export async function handle(method, path, queryString, headers, body) {
   console.log('');
   console.log('= REQUEST =====================');
