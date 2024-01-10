3ebe7da0541261a3170b04e5cc4cfc3eea3cd453
Richard Olsson <r@richardolsson.se>
2024-01-10 11:11:03 +0100

    Add simple access control using headers

--- classes/a502/examples/http-io/io.js
+++ classes/a502/examples/http-io/io.js
@@ -13,19 +13,26 @@ export async function handle(
   console.log('HEADERS', headers);
   console.log('BODY', body);
 
-  if (path == '/hello') {
-    return {
-      body: '<html><body><h1>Hello, world!</h1></body></html>',
-      status: 200,
-    };
-  } else if (path == '/goodbye') {
-    return {
-      body: '<html><body><h1>Goodbye, world!</h1></body></html>',
-      status: 200,
-    };
+  if (headers.authorization == 'mypassword') {
+    if (path == '/hello') {
+      return {
+        body: '<html><body><h1>Hello, world!</h1></body></html>',
+        status: 200,
+      };
+    } else if (path == '/goodbye') {
+      return {
+        body: '<html><body><h1>Goodbye, world!</h1></body></html>',
+        status: 200,
+      };
+    } else {
+      return {
+        status: 404,
+      };
+    }
   } else {
     return {
-      status: 404,
+      body: '<html><body><h1>ACCESS DENIED!</h1></body></html>',
+      status: 403,
     };
   }
 }
