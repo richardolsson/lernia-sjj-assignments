fda4fe124009c2f6e6a219c7339bc5ab2910e2b8
Richard Olsson <r@richardolsson.se>
2024-01-18 13:56:42 +0100

    Add dynamic active/inactive links to menu

--- classes/a504/examples/ex2-express/index.js
+++ classes/a504/examples/ex2-express/index.js
@@ -22,8 +22,15 @@ async function renderPage(response, page) {
   const buf = await fs.readFile(`./templates/main.html`);
   const html = buf.toString();
 
+  // Index page has '/' link, but 'index' page name, so it
+  // needs special treatment. In other cases, the current path
+  // is just the page name preceded by /.
+  const currentPath = (page == 'index') ? '/' : `/${page}`;
+
   const menuItemStrings = MENU.map((item) => {
-    return `<li><a href="${item.link}">${item.label}</a></li>`;
+    const className = (item.link == currentPath) ? 'active' : 'inactive';
+
+    return `<li class="menu-item ${className}"><a href="${item.link}">${item.label}</a></li>`;
   });
 
   const menuString = menuItemStrings.join('\n');
