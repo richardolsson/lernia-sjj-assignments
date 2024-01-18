e60077f105f3df6bbd7aca2779f273448c247475
Richard Olsson <r@richardolsson.se>
2024-01-18 13:42:06 +0100

    Generate menu HTML in backend code

--- classes/a504/examples/ex2-express/index.js
+++ classes/a504/examples/ex2-express/index.js
@@ -3,14 +3,35 @@ import fs from 'fs/promises';
 
 const app = express();
 
+const MENU = [
+  {
+    label: 'Home page',
+    link: '/',
+  },
+  {
+    label: 'About us',
+    link: '/about',
+  },
+  {
+    label: 'Contact us',
+    link: '/contact',
+  },
+];
+
 async function renderPage(response, page) {
   const buf = await fs.readFile(`./templates/main.html`);
   const html = buf.toString();
 
+  const menuItemStrings = MENU.map((item) => {
+    return `<li><a href="${item.link}">${item.label}</a></li>`;
+  });
+
+  const menuString = menuItemStrings.join('\n');
+
   const contentBuf = await fs.readFile(`./content/${page}.html`);
   const contentHtml = contentBuf.toString();
 
-  const rendered = html.replace('%%content%%', contentHtml);
+  const rendered = html.replace('%%content%%', contentHtml).replace('#menu#', menuString);
 
   response.send(rendered);
 }
