8839045590b7f7e3b9eafb644ce00928c2ba5073
Richard Olsson <r@richardolsson.se>
2024-01-24 10:14:14 +0100

    Simplify data objects

--- classes/a507/examples/tested-kino-website/src/movies.js
+++ classes/a507/examples/tested-kino-website/src/movies.js
@@ -5,11 +5,20 @@ const API_BASE = 'https://plankton-app-xhkom.ondigitalocean.app/api';
 export async function loadMovies() {
   const res = await fetch(API_BASE + '/movies');
   const payload = await res.json();
-  return payload.data;
+  const modifiedArray = payload.data.map((obj) => {
+    return {
+      id: obj.id,
+      ...obj.attributes,
+    };
+  });
+  return modifiedArray;
 }
 
 export async function loadMovie(id) {
   const res = await fetch(API_BASE + '/movies/' + id);
   const payload = await res.json();
-  return payload.data;
+  return {
+    id: payload.data.id,
+    ...payload.data.attributes,
+  };
 }
