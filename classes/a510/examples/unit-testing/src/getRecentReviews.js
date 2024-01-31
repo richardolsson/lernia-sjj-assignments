df538d8caa7aa8f49352c3b367b65ad9ea04c59f
Richard Olsson <r@richardolsson.se>
2024-01-31 11:00:24 +0100

    Load reviews from CMS

--- classes/a510/examples/unit-testing/src/getRecentReviews.js
+++ classes/a510/examples/unit-testing/src/getRecentReviews.js
@@ -1,2 +1,5 @@
 export default async function getRecentReviews() {
+    const response = await fetch('https://plankton-app-xhkom.ondigitalocean.app/api/reviews?populate=movie');
+    const payload = await response.json();
+    return payload.data;
 }
