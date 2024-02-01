fbacd4bec803a1bf9f8bc884fd3ad88859496f73
Richard Olsson <r@richardolsson.se>
2024-02-01 15:22:29 +0100

    Add code for basic auth

--- classes/a511/examples/ex3-password-protected/index.js
+++ classes/a511/examples/ex3-password-protected/index.js
@@ -6,6 +6,27 @@ const USERNAME = 'admin';
 const PASSWORD = 'secret';
 
 app.post('/api/login', (req, res) => {
+  const authHeader = req.headers.authorization;
+  const b64credentials = authHeader?.slice(6);
+  let username, password;
+
+  try {
+    const credentials = atob(b64credentials);
+    [username, password] = credentials.split(':');
+  } catch (err) {
+    username = '';
+    password = '';
+  }
+
+  if (username == USERNAME && password == PASSWORD) {
+    res.status(200).json({
+      ok: true,
+    });
+  } else {
+    res.status(401).json({
+      ok: false,
+    });
+  }
 });
 
 app.get('/api/protected', (req, res) => {
