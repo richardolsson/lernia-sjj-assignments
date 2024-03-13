35aad580c3fba5162ef810e3fe1496c68becd66d
Richard Olsson <r@richardolsson.se>
2024-03-13 11:14:41 +0100

    Implement first parts of title case algorithm using TDD

--- classes/b702/examples/titlecase.js
+++ classes/b702/examples/titlecase.js
@@ -1,2 +1,8 @@
 export default function titleCase(str) {
+  const words = str.split(' ');
+  for (let i = 0; i < words.length; i++) {
+    words[i] = words[i].charAt(0).toUpperCase() + words[i].slice(1);
+  }
+
+  return words.join(' ');
 }
