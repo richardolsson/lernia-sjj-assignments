b6a00b7ac42054d18171baf69a149c575d3326af
Richard Olsson <r@richardolsson.se>
2024-03-20 11:08:27 +0100

    Add state to TaskInput component

--- classes/b704/examples/react-components/src/components/TaskInput.js
+++ classes/b704/examples/react-components/src/components/TaskInput.js
@@ -1,9 +1,15 @@
+import { useState } from 'react';
+
 export default function TaskInput() {
+  const [text, setText] = useState('');
+
   return (
     <form className="todoForm" onSubmit={(ev) => {
       ev.preventDefault();
     }}>
-      <input className="todoForm__input" type="text" onChange={(ev) => { }} />
+      <input className="todoForm__input" type="text" value={text} onChange={(ev) => {
+        setText(ev.target.value);
+      }} />
       <button className="todoForm__submitButton" type="submit">OK</button>
     </form>
   );
