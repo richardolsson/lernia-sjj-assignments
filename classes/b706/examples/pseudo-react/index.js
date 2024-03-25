f6695e74080452be2a7a325b20be2d4c6fc19e6c
Richard Olsson <r@richardolsson.se>
2024-03-25 10:49:14 +0100

    Create imperative and declarative examples using normal DOM

---
+++ classes/b706/examples/pseudo-react/index.js
@@ -0,0 +1,111 @@
+//initImperative();
+//initDeclarative();
+
+/**
+ * This function initiates the interface using a purely "imperative"
+ * approach, the same way we have in previous projects. Every time the
+ * state changes, we also have to make updates to the UI. Beacuse state
+ * changes from multiple places, we also have to update the UI from all
+ * of those places.
+ * 
+ * Pros:
+ * - Very fast
+ * 
+ * Cons:
+ * - Some duplicate code
+ * - Easy to make mistakes, especially when making changes to design
+ */
+function initImperative() {
+  let num = 0;
+
+  const p = document.createElement('p');
+  p.textContent = num;
+
+  const button = document.createElement('button');
+  button.textContent = 'Click me to increment';
+  button.addEventListener('click', () => {
+    num += 1;
+
+    // State (num) changed, so we have to update UI
+    p.textContent = num;
+    button.textContent = `Click me again (${num})`;
+    resetButton.style.display = 'inline-block';
+  });
+
+  const resetButton = document.createElement('button');
+  resetButton.textContent = 'Reset';
+  resetButton.style.display = 'none';
+  resetButton.addEventListener('click', () => {
+    num = 0;
+
+    // State (num) changed, so we have to update UI
+    p.textContent = num;
+    button.textContent = 'Click me to increment';
+    resetButton.style.display = 'none';
+  });
+
+  const container = document.querySelector('#app');
+  container.append(p);
+  container.append(button);
+  container.append(resetButton);
+}
+
+
+/**
+ * This function initiates the interface using a "declarative" approach,
+ * where the interface is declared to look a certain way for the current
+ * state, and then is re-rendered from scratch whenever the state changes,
+ * by emptying the DOM and building all of the elements again.
+ * 
+ * Pros:
+ * - Easy for the programmer not to make mistakes
+ * 
+ * Cons:
+ * - Slow performance
+ * - Usability issues like losing focus when an element is removed
+ */
+function initDeclarative() {
+  let num = 0;
+
+  render();
+
+  function render() {
+    // Empty everything and start from scratch
+    const container = document.querySelector('#app');
+    container.innerHTML = '';
+
+    const p = document.createElement('p');
+    p.textContent = num;
+
+    const button = document.createElement('button');
+    button.addEventListener('click', () => {
+      num += 1;
+
+      // State (num) changed, so we have to update UI
+      render();
+    });
+
+    // Conditional rendering based on state
+    button.textContent = (num == 0)
+      ? 'Click me to increment'
+      : `Click me again (${num})`;
+
+    const resetButton = document.createElement('button');
+    resetButton.textContent = 'Reset';
+    resetButton.addEventListener('click', () => {
+      num = 0;
+
+      // State (num) changed, so we have to update UI
+      render();
+    });
+
+    // Conditional rendering based on state
+    resetButton.style.display = (num == 0)
+      ? 'none'
+      : 'display-block';
+
+    container.append(p);
+    container.append(button);
+    container.append(resetButton);
+  }
+}
