f7f80ec8e04c8a4c43321108eb7737c05b991449
Richard Olsson <r@richardolsson.se>
2024-03-25 13:22:49 +0100

    Implement very simple API

--- classes/b706/examples/fullstack-react/backend/index.js
+++ classes/b706/examples/fullstack-react/backend/index.js
@@ -2,9 +2,20 @@ import express from 'express';
 
 
 const app = express();
+app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.send('Hello, world!');
+const items = [];
+
+app.get('/api/items', (req, res) => {
+  res.json({ items });
+});
+
+app.post('/api/items', (req, res) => {
+  const item = req.body
+  console.log(req.body);
+  items.push(item);
+
+  res.status(201).json(item);
 });
 
 app.listen(5080);
