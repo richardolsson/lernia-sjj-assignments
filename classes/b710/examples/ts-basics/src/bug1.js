4818d3b9cbdef53c619595922137b27967123da7
Richard Olsson <r@richardolsson.se>
2024-04-08 08:17:44 +0200

    Add JS examples for TS class

---
+++ classes/b710/examples/ts-basics/src/bug1.js
@@ -0,0 +1,11 @@
+export default function bug1() {
+  const data = {
+    data: {
+      data: ["word1", "word2"],
+    },
+  };
+
+  data.data.push("word3");
+}
+
+bug1();
---
+++ classes/b710/examples/ts-basics/src/bug2.js
@@ -0,0 +1,13 @@
+export default function bug2() {
+  function add100(x) {
+    return x + 100;
+  }
+
+  const str = "What is 50 + 100?";
+  const words = str.split(" ");
+  const val = words[2];
+  const result = add100(val);
+  console.log("Result is " + result);
+}
+
+bug2();
