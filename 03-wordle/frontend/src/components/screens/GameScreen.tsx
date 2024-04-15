de960eaefccb63fbdaa7fba223237f1fcc72e7b9
Richard Olsson <r@richardolsson.se>
2024-04-15 11:31:34 +0200

    Create dummy screens and logic to start game

---
+++ 03-wordle/frontend/src/components/screens/GameScreen.tsx
@@ -0,0 +1,7 @@
+import { FC } from 'react';
+
+const GameScreen: FC = () => {
+  return <h1>Game</h1>;
+};
+
+export default GameScreen;
---
+++ 03-wordle/frontend/src/components/screens/WinScreen.tsx
@@ -0,0 +1,7 @@
+import { FC } from 'react';
+
+const WinScreen: FC = () => {
+  return <h1>WIN!</h1>;
+};
+
+export default WinScreen;
