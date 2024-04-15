a671f1efbc8415a0d09e886ce8803c6f16856033
Richard Olsson <r@richardolsson.se>
2024-04-15 10:18:49 +0200

    Create ListWordRandomizer to pick random word from list

--- 03-wordle/backend/src/game/types.ts
+++ 03-wordle/backend/src/game/types.ts
@@ -1,10 +1,14 @@
 export type Game = {
   id: string;
   correctWord: string;
-  wordLength: number,
-  allowDuplicates: boolean,
-}
+  wordLength: number;
+  allowDuplicates: boolean;
+};
 
 export interface IGameStore {
   createGame(wordLength: number, allowDuplicates: boolean): Game;
+}
+
+export interface IWordRandomizer {
+  getRandomWord(wordLength: number, allowDuplicates: boolean): string;
 }
