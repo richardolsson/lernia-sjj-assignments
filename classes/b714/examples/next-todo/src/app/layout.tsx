76b1a7ce4ec2d77c32967c0be70e3c40f1131073
Richard Olsson <r@richardolsson.se>
2024-04-17 11:53:42 +0200

    Add navbar example to layout

--- classes/b714/examples/next-todo/src/app/layout.tsx
+++ classes/b714/examples/next-todo/src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import './globals.css';
+import Link from 'next/link';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -16,7 +17,14 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <div>
+          <Link href="/">Home</Link>
+          {' | '}
+          <Link href="/about">About</Link>
+        </div>
+        {children}
+      </body>
     </html>
   );
 }
