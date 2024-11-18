4c340e4fd09874c28f85bbcfcb6f31f9f3f18274
Richard Olsson <r@richardolsson.se>
2024-11-18 11:39:36 +0100

    Add module example

---
+++ classes/a3/a310/examples/modules/main.js
@@ -0,0 +1,6 @@
+import { start as firstStart } from './first.js';
+
+import secondStart from './second.js';
+
+firstStart();
+secondStart();
---
+++ classes/a3/a310/examples/modules/second.js
@@ -0,0 +1,5 @@
+import { x } from './first.js';
+
+export default function start() {
+  console.log('SECOND', x);
+}
