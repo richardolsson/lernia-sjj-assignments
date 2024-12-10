e943f0eef7c491aa1c6a9cbf091f61cca48ead5d
Richard Olsson <r@richardolsson.se>
2024-12-10 15:55:36 +0100

    Add toggleVisibility function to Challenge

--- 02-esc/src/js/Challenge.js
+++ 02-esc/src/js/Challenge.js
@@ -6,38 +6,42 @@ export default class Challenge {
   }
 
   render(elemFactory) {
-    const container = elemFactory.createElement('article');
-    container.className = 'challenge';
+    this.container = elemFactory.createElement('article');
+    this.container.className = 'challenge';
 
     const img = elemFactory.createElement('img');
     img.className = 'challenge__image';
     img.src = this.data.image;
-    container.append(img);
+    this.container.append(img);
 
     const h3 = elemFactory.createElement('h3');
     h3.className = 'challenge__title';
     h3.textContent = this.data.title;
-    container.append(h3);
+    this.container.append(h3);
 
     const rating = new Rating(this.data.rating, 'challenge__rating');
     const ratingElem = rating.render(elemFactory);
-    container.append(ratingElem);
+    this.container.append(ratingElem);
 
     const groupSize = elemFactory.createElement('span');
     groupSize.className = 'challenge__groupSize';
     groupSize.textContent = `${this.data.minParticipants}-${this.data.maxParticipants} participants`;
-    container.append(groupSize);
+    this.container.append(groupSize);
 
     const description = elemFactory.createElement('p');
     description.className = 'challenge__description';
     description.textContent = this.data.description;
-    container.append(description);
+    this.container.append(description);
 
     const button = elemFactory.createElement('button');
     button.className = 'challenge__bookButton';
     button.textContent = this.data.type == 'online' ? 'Take challenge online' : 'Book this room';
-    container.append(button);
+    this.container.append(button);
 
-    return container;
+    return this.container;
+  }
+
+  toggleVisibility(visible) {
+    this.container.style.opacity = visible ? 1 : 0.5;
   }
 }
