b467103b526c355b9eeb80b8fde9801d7abfe752
Richard Olsson <r@richardolsson.se>
2024-12-10 15:34:44 +0100

    Set up testing

---
+++ 02-esc/src/js/testing/mockChallengeData.js
@@ -0,0 +1,14 @@
+export default function mockChallengeData(overrides) {
+  return {
+    id: 1,
+    title: 'My challenge',
+    description: 'This is a challenge',
+    rating: 3,
+    labels: ['foo', 'bar'],
+    image: 'https://example.com/example.png',
+    minParticipants: 2,
+    maxParticipants: 5,
+    type: 'onsite',
+    ...overrides,
+  };
+}
