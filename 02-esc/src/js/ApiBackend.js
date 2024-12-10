2d681f4c4f0178a8c0f5bb3b55bd1be42ba5b1d2
Richard Olsson <r@richardolsson.se>
2024-12-10 17:51:16 +0100

    Add API calls for booking modal

--- 02-esc/src/js/ApiBackend.js
+++ 02-esc/src/js/ApiBackend.js
@@ -8,4 +8,24 @@ export default class ApiBackend {
     const payload = await response.json();
     return payload.challenges;
   }
+
+  async loadAvailableTimes(challenge, date) {
+    const url = `${this.apiUrl}/booking/available-times?date=${date}&challenge=${challenge.data.id}`;
+    const response = await fetch(url);
+    const payload = await response.json();
+    return payload.slots;
+  }
+
+  async submitBooking(booking) {
+    const url = this.apiUrl + '/booking/reservations';
+    const response = await fetch(url, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(booking),
+    });
+    const payload = await response.json();
+    return payload.booking;
+  }
 }
