57bf14cd63054fa66c543f4f8d22d30b542bd0d7
Richard Olsson <r@richardolsson.se>
2025-01-08 11:05:35 +0100

    Add simple "fake database"

--- classes/a5/a502/examples/rest-vs-rpc/io.js
+++ classes/a5/a502/examples/rest-vs-rpc/io.js
@@ -1,3 +1,19 @@
+const people = [
+  { id: 1, name: 'Artemis N Folkmore', profession: 'President' },
+  { id: 2, name: 'Yev Kassim', profession: 'Restaurateur' },
+];
+function getPeople() {
+  return people;
+}
+function getPerson(personId) {
+  return people.find(person => person.id == personId);
+}
+function setName(personId, name) {
+  const person = getPerson(personId);
+  person.name = name;
+  return person;
+}
+
 export async function handle(method, path, queryString, headers, body) {
   console.log('');
   console.log('= REQUEST =====================');
