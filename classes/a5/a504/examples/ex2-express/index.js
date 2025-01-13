b5a641c9af22615d2be895b4424d4db240187d61
Richard Olsson <r@richardolsson.se>
2025-01-13 15:32:37 +0100

    Move renderPage() to separate module

--- classes/a5/a504/examples/ex2-express/index.js
+++ classes/a5/a504/examples/ex2-express/index.js
@@ -1,46 +1,7 @@
 import express from 'express';
-import fs from 'fs/promises';
+import renderPage from './lib/renderPage.js';
 
 const app = express();
-
-const MENU = [
-  {
-    label: 'Home page',
-    id: 'index',
-    link: '/',
-  },
-  {
-    label: 'About us',
-    id: 'about',
-    link: '/about',
-  },
-  {
-    label: 'Contact us',
-    id: 'contact',
-    link: '/contact',
-  }
-];
-
-async function renderPage(response, page) {
-  const contentBuf = await fs.readFile(`./content/${page}.html`);
-  const contentText = contentBuf.toString();
-
-  const templateBuf = await fs.readFile('./templates/main.html');
-  const templateText = templateBuf.toString();
-
-  const htmlItems = MENU.map((item) => {
-    const className = item.id == page? 'active' : 'inactive';
-    return `<li class="menu-item ${className}"><a href="${item.link}">${item.label}</a></li>`;
-  });
-
-  const menuText = htmlItems.join('\n');
-
-  const outputHtml = templateText
-    .replace('=#content#=', contentText)
-    .replace('%%menu%%', menuText);
-
-  response.send(outputHtml);
-}
 
 app.get('/', (request, response) => {
   renderPage(response, 'index');
