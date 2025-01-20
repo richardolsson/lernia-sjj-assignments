f84ccc42a2e398e3117d145e847a3a0e075c16ed
Richard Olsson <r@richardolsson.se>
2025-01-20 15:21:30 +0100

    Refactor shape of API objects

--- classes/a5/a507/examples/tested-kino-website/src/movies.js
+++ classes/a5/a507/examples/tested-kino-website/src/movies.js
@@ -2,14 +2,21 @@ import fetch from "node-fetch";
 
 const API_BASE = 'https://plankton-app-xhkom.ondigitalocean.app/api';
 
+function toMovieObject(apiObject) {
+  return {
+    id: apiObject.id,
+    ...apiObject.attributes,
+  }
+}
+
 export async function loadMovies() {
   const res = await fetch(API_BASE + '/movies');
   const payload = await res.json();
-  return payload.data;
+  return payload.data.map(toMovieObject);
 }
 
 export async function loadMovie(id) {
   const res = await fetch(API_BASE + '/movies/' + id);
   const payload = await res.json();
-  return payload.data;
+  return toMovieObject(payload.data);
 }
