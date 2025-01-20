324cf4c77e9358e1ae4a4e9c2bddc14270172112
Richard Olsson <r@richardolsson.se>
2025-01-20 15:47:18 +0100

    Add mocked API for test

--- classes/a5/a507/examples/tested-kino-website/tests/movies.test.js
+++ classes/a5/a507/examples/tested-kino-website/tests/movies.test.js
@@ -1,13 +1,29 @@
 import { expect, test } from '@jest/globals';
 import request from 'supertest';
 
-import { loadMovie, loadMovies } from '../src/movies.js';
 import initApp from '../src/app.js';
 
 test('Home page shows list of movies', async () => {
   const app = initApp({
-    loadMovie,
-    loadMovies,
+    loadMovie: async () => ({
+      id: 1,
+      title: 'Encanto',
+      // ...
+    }),
+    loadMovies: async () => [
+      {
+        id: 1,
+        title: 'Encanto',
+      },
+      {
+        id: 2,
+        title: 'Forrest Gump',
+      },
+      {
+        id: 3,
+        title: 'Training Day',
+      }
+    ],
   });
   const response = await request(app)
     .get('/')
