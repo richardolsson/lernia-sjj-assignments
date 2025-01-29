b37a22b3dae56615116b323b17d30d83d528f5e9
Richard Olsson <r@richardolsson.se>
2025-01-29 12:02:36 +0100

    Implement logic and add tests for getRecentReviews()

--- classes/a5/a511/examples/unit-testing/src/getRecentReviews.js
+++ classes/a5/a511/examples/unit-testing/src/getRecentReviews.js
@@ -1,7 +1,17 @@
 export default async function getRecentReviews(cmsAdapter) {
   const reviews = await cmsAdapter.loadAllReviews();
 
-  // TODO: logic goes here
+  const limit = new Date();
+  limit.setDate(-60);
 
-  return reviews;
+  const positiveReviews = reviews.filter((review) => {
+    const reviewDate = new Date(review.attributes.createdAt);
+    return review.attributes.rating >= 3 && reviewDate > limit;
+  });
+
+  if (positiveReviews.length >= 3) {
+    return positiveReviews;
+  } else {
+    return [];
+  }
 }
