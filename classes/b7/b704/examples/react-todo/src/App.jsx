4f13efdd030c442012a6b3293d6232f4d209e250
Richard Olsson <r@richardolsson.se>
2025-03-24 15:27:04 +0100

    Add hard-coded todo list

--- classes/b7/b704/examples/react-todo/src/App.jsx
+++ classes/b7/b704/examples/react-todo/src/App.jsx
@@ -1,7 +1,29 @@
 
 function App() {
   return (
-    <h1>Hello</h1>
+    <main class="app">
+      <h1 class="app__header">My ToDo</h1>
+      <p class="app__status">1 completed</p>
+      <form class="todoForm">
+        <input type="text" class="todoForm__input" />
+        <button type="submit" class="todoForm__okButton">OK</button>
+        <small class="todoForm__error">Input must not be empty</small>
+      </form>
+      <ul class="todoList">
+        <li class="todoItem todoItem--completed">
+          <span class="todoItem__label">Learn HTML</span>
+          <button class="todoItem__deleteButton">🗑️</button>
+        </li>
+        <li class="todoItem">
+          <span class="todoItem__label">Learn JS</span>
+          <button class="todoItem__deleteButton">🗑️</button>
+        </li>
+        <li class="todoItem">
+          <span class="todoItem__label">Learn React</span>
+          <button class="todoItem__deleteButton">🗑️</button>
+        </li>
+      </ul>
+    </main>
   )
 }
 
