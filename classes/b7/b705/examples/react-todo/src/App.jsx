3c3532ab720b5349f69922321c3a19da50074291
Richard Olsson <r@richardolsson.se>
2025-03-26 07:13:52 +0100

    Add error handling when submitting empty string

--- classes/b7/b705/examples/react-todo/src/App.jsx
+++ classes/b7/b705/examples/react-todo/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react';
 
 function App() {
   const [text, setText] = useState('');
+  const [error, setError] = useState(false);
   const [items, setItems] = useState([
     {
       label: 'Learn HTML',
@@ -17,22 +18,34 @@ function App() {
     }
   ]);
 
+  let formClass = 'todoForm';
+  if (error) {
+    formClass += ' todoForm--withError';
+  }
+
   return (
     <main className="app">
       <h1 className="app__header">My ToDo</h1>
       <p className="app__status">{items.filter(item => item.completed).length} completed</p>
-      <form className="todoForm" onSubmit={(ev) => {
+      <form className={formClass} onSubmit={(ev) => {
         ev.preventDefault();
-        setItems([
-          ...items,
-          {
-            label: text,
-            completed: false,
-          },
-        ]);
-        setText('');
+        if (text.length > 0) {
+          setItems([
+            ...items,
+            {
+              label: text,
+              completed: false,
+            },
+          ]);
+          setText('');
+        } else {
+          setError(true);
+        }
       }}>
         <input type="text" className="todoForm__input" value={text} onChange={(ev) => {
+          if (error) {
+            setError(false);
+          }
           setText(ev.target.value);
         }} />
         <button type="submit" className="todoForm__okButton">OK</button>
