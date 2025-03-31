0a5fa0e367cde90990dcc5415288e845ccb65275
Richard Olsson <r@richardolsson.se>
2025-03-31 15:24:54 +0200

    Set up API proxy in vite dev server

--- classes/b7/b707/examples/fullstack-todo/frontend/vite.config.js
+++ classes/b7/b707/examples/fullstack-todo/frontend/vite.config.js
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/api': 'http://localhost:5080'
+    },
+  },
 })
