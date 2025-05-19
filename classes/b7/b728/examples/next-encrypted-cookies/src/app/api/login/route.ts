2218d27561445309d28454cfccd076913d12569d
Richard Olsson <r@richardolsson.se>
2025-05-19 11:47:31 +0200

    Implement encrypted cookie using @hapi/iron

--- classes/b7/b728/examples/next-encrypted-cookies/src/app/api/login/route.ts
+++ classes/b7/b728/examples/next-encrypted-cookies/src/app/api/login/route.ts
@@ -1,10 +1,23 @@
+import Iron from "@hapi/iron";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function POST(request: NextRequest) {
   const payload = await request.json();
   if (payload.username == "admin" && payload.password == "secretpass") {
     const response = new NextResponse();
-    response.cookies.set("username", payload.username);
+
+    const sessionData = {
+      username: payload.username,
+      loginTime: new Date().toISOString(),
+    };
+
+    const sealed = await Iron.seal(
+      sessionData,
+      process.env.ENC_KEY!,
+      Iron.defaults
+    );
+
+    response.cookies.set("session", sealed);
     return response;
   } else {
     return NextResponse.json({}, { status: 401 });
--- classes/b7/b728/examples/next-encrypted-cookies/src/app/secret/page.tsx
+++ classes/b7/b728/examples/next-encrypted-cookies/src/app/secret/page.tsx
@@ -1,13 +1,22 @@
 import { cookies } from "next/headers";
+import Iron from "@hapi/iron";
 
 export default async function SecretPage() {
   const allCookies = await cookies();
 
-  const username = allCookies.get("username")?.value ?? null;
-
-  if (username) {
-    return <h1>Hello {username}, this is secret!</h1>;
-  } else {
+  const sessionString = allCookies.get("session")?.value ?? "";
+  let unsealed;
+  try {
+    unsealed = await Iron.unseal(
+      sessionString,
+      process.env.ENC_KEY!,
+      Iron.defaults
+    );
+  } catch (err) {
     return <h1>Nothing here!</h1>;
   }
+
+  const username = unsealed.username;
+
+  return <h1>Hello {username}, this is secret!</h1>;
 }
