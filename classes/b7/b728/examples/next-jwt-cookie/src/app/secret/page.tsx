904891d8768d76df32fb6ad3f56a9188a5a0155f
Richard Olsson <r@richardolsson.se>
2025-05-19 12:04:10 +0200

    Implement session cookie using JWT

--- classes/b7/b728/examples/next-jwt-cookie/src/app/secret/page.tsx
+++ classes/b7/b728/examples/next-jwt-cookie/src/app/secret/page.tsx
@@ -1,13 +1,18 @@
 import { cookies } from "next/headers";
+import jsonwebtoken from "jsonwebtoken";
 
 export default async function SecretPage() {
   const allCookies = await cookies();
 
-  const username = allCookies.get("username")?.value ?? null;
+  const sessionString = allCookies.get("session")?.value ?? "";
 
-  if (username) {
-    return <h1>Hello {username}, this is secret!</h1>;
-  } else {
+  try {
+    const session = jsonwebtoken.verify(sessionString, process.env.ENC_KEY!);
+    if (typeof session == "object") {
+      const username = session.username;
+      return <h1>Hello {username}, this is secret!</h1>;
+    }
+  } catch (err) {
     return <h1>Nothing here!</h1>;
   }
 }
