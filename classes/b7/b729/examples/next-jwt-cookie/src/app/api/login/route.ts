f883c58903e5f027efbed6b51ec4a544b4e0ad93
Richard Olsson <r@richardolsson.se>
2025-05-21 11:34:21 +0200

    Use bcrypt in NEXT.js app

--- classes/b7/b729/examples/next-jwt-cookie/src/app/api/login/route.ts
+++ classes/b7/b729/examples/next-jwt-cookie/src/app/api/login/route.ts
@@ -1,20 +1,41 @@
+import bcrypt from "bcrypt";
 import { NextRequest, NextResponse } from "next/server";
-import jsonwebtoken from 'jsonwebtoken';
+import jsonwebtoken from "jsonwebtoken";
+
+// This should really be in a database
+const users = [
+  {
+    username: "admin",
+    passwordHash:
+      "$2b$14$UVE0oY.BzMEjaENbOQi54.QUSm4K8ZXiG/zix81aTT9rkMPnpsoW.",
+  },
+];
 
 export async function POST(request: NextRequest) {
   const payload = await request.json();
-  if (payload.username == "admin" && payload.password == "secretpass") {
-    const sessionData = {
-      username: payload.username,
-      loginTime: new Date().toISOString(),
-    };
+  const user = users.find(
+    (candidate) => candidate.username == payload.username
+  );
 
-    const jwt = jsonwebtoken.sign(sessionData, process.env.ENC_KEY!);
+  if (user) {
+    const passwordIsCorrect = await bcrypt.compare(
+      payload.password,
+      user.passwordHash
+    );
 
-    const response = new NextResponse();
-    response.cookies.set("session", jwt);
-    return response;
-  } else {
-    return NextResponse.json({}, { status: 401 });
+    if (passwordIsCorrect) {
+      const sessionData = {
+        username: payload.username,
+        loginTime: new Date().toISOString(),
+      };
+
+      const jwt = jsonwebtoken.sign(sessionData, process.env.ENC_KEY!);
+
+      const response = new NextResponse();
+      response.cookies.set("session", jwt);
+      return response;
+    }
   }
+
+  return NextResponse.json({}, { status: 401 });
 }
