8ea4dbfaa53723ba58e6843198166187ccbbd99e
Richard Olsson <r@richardolsson.se>
2025-06-02 15:01:02 +0200

    Add Switch which uses native component from each platform

--- classes/b7/b734/react-native-example/app/(tabs)/index.tsx
+++ classes/b7/b734/react-native-example/app/(tabs)/index.tsx
@@ -1,12 +1,14 @@
 import { Image } from 'expo-image';
-import { Platform, StyleSheet } from 'react-native';
+import { Platform, StyleSheet, Switch } from 'react-native';
 
 import { HelloWave } from '@/components/HelloWave';
 import ParallaxScrollView from '@/components/ParallaxScrollView';
 import { ThemedText } from '@/components/ThemedText';
 import { ThemedView } from '@/components/ThemedView';
+import { useState } from 'react';
 
 export default function HomeScreen() {
+  const [value, setValue] = useState(false);
   return (
     <ParallaxScrollView
       headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
@@ -20,6 +22,11 @@ export default function HomeScreen() {
         <ThemedText type="title">Welcome!</ThemedText>
         <HelloWave />
       </ThemedView>
+      <Switch
+        value={value}
+        onValueChange={(newValue) => setValue(newValue)}
+        style={styles.switch}
+      />
       <ThemedView style={styles.stepContainer}>
         <ThemedText type="subtitle">Step 1: Try it</ThemedText>
         <ThemedText>
@@ -71,5 +78,8 @@ const styles = StyleSheet.create({
     bottom: 0,
     left: 0,
     position: 'absolute',
+  },
+  switch: {
+    margin: 100,
   },
 });
