de40b4f3e978f327149283c71def4a76b4f77270
Richard Olsson <r@richardolsson.se>
2025-10-13 10:08:54 +0200

    Add error message when input is empty

--- livecoding/01-todo/main.js
+++ livecoding/01-todo/main.js
@@ -1,8 +1,16 @@
 const form = document.querySelector('.taskForm');
 form.addEventListener('submit', (event) => {
   event.preventDefault();
+  const errorElement = document.querySelector('.taskForm__error');
   const inputElement = document.querySelector('.taskForm__input');
   const inputText = inputElement.value;
+
+  if (inputText.length == 0) {
+    errorElement.textContent = 'Input must not be empty';
+    return;
+  }
+
+  errorElement.textContent = '';
 
   const itemElement = document.createElement('li');
   itemElement.className = 'taskList__item';
