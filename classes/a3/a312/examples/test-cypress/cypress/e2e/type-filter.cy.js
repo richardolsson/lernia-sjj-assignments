8660857bbc888028bf5f0aa1366b5adc6cc82b1c
Richard Olsson <r@richardolsson.se>
2025-11-24 13:20:02 +0100

    Add end-to-end (e2e) test using cypress

--- classes/a3/a312/examples/test-cypress/cypress/e2e/type-filter.cy.js
+++ classes/a3/a312/examples/test-cypress/cypress/e2e/type-filter.cy.js
@@ -1,5 +1,20 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
+describe('filter page', () => {
+  it('filters online challenges', () => {
+    cy.visit('http://localhost:5500/classes/a3/a312/examples/test-manual/index.html');
+    cy.get('button').contains('online').click();
+    cy.get('ul li').should('have.length', 15);
   })
+
+  it('filters onsite challenges', () => {
+    cy.visit('http://localhost:5500/classes/a3/a312/examples/test-manual/index.html');
+    cy.get('button').contains('onsite').click();
+    cy.get('ul li').should('have.length', 15);
+  });
+
+  it('filters online challenges after onsite', () => {
+    cy.visit('http://localhost:5500/classes/a3/a312/examples/test-manual/index.html');
+    cy.get('button').contains('onsite').click();
+    cy.get('button').contains('online').click();
+    cy.get('ul li').should('have.length', 15);
+  });
 })
