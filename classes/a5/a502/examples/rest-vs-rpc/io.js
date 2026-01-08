9248d398420edfcdbbc68233fe8a541b349a248b
Richard Olsson <r@richardolsson.se>
2026-01-08 15:29:08 +0100

    Implement RPC server

--- classes/a5/a502/examples/rest-vs-rpc/io.js
+++ classes/a5/a502/examples/rest-vs-rpc/io.js
@@ -1,2 +1,67 @@
+const people = [
+  { id: 1, name: 'Artemis N Folkmore', profession: 'President' },
+  { id: 2, name: 'Yev Kassim', profession: 'Restaurateur' },
+];
+
+function getPeople() {
+  return people;
+}
+
+function getPersonById(personId) {
+  return people.find(person => person.id == personId);
+}
+
+function setName(personId, name) {
+  const person = getPersonById(personId);
+  person.name = name;
+  return person;
+}
+
 export async function handle(method, path, queryString, headers, body) {
+  console.log('');
+  console.log('= REQUEST =====================');
+  console.log('method:  ', method);
+  console.log('path:    ', path);
+  console.log('query:   ', queryString);
+  console.log('headers: ', headers);
+  console.log('body:    ', body);
+
+  return handleRPC(method, path, queryString, headers, body);
+}
+
+async function handleRPC(method, path, queryString, headers, body) {
+  if (method == 'POST' && path == '/') {
+    const { funcName, params } = body;
+
+    if (funcName == 'getPeople') {
+      const result = getPeople();
+
+      return {
+        status: 200,
+        body: {
+          result,
+        }
+      };
+    } else if (funcName == 'getPerson') {
+      const personId = params[0];
+      const result = getPersonById(personId);
+
+      return {
+        status: 200,
+        body: {
+          result,
+        }
+      };
+    } else if (funcName == 'setPersonName') {
+      const [personId, newName] = params;
+      const result = setName(personId, newName);
+
+      return {
+        status: 200,
+        body: {
+          result,
+        }
+      };
+    }
+  }
 }
