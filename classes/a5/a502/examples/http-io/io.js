0891b07dda7da034d112296284ef9a617d364891
Richard Olsson <r@richardolsson.se>
2026-01-08 14:36:51 +0100

    Create example of using IO to build simple website

--- classes/a5/a502/examples/http-io/io.js
+++ classes/a5/a502/examples/http-io/io.js
@@ -1,2 +1,38 @@
 export async function handle(method, path, queryString, headers, body) {
+  console.log('');
+  console.log('= REQUEST =====================');
+  console.log('method:  ', method);
+  console.log('path:    ', path);
+  console.log('query:   ', queryString);
+  console.log('headers: ', headers);
+  console.log('body:    ', body);
+
+  if (path == '/') {
+    return {
+      status: 200,
+      body: createHtml('<h1>Hello, world!</h1><a href="/goodbye">Goodbye</a></body>')
+    };
+  } else if (path == '/goodbye') {
+    return {
+      status: 200,
+      body: createHtml('<h1>Goodbye, world!</h1><a href="/">Hello</a></body>')
+    }
+  } else if (path == '/style.css') {
+    return {
+      status: 200,
+      body: 'h1 { color: red; }',
+    }
+  } else {
+    return {
+      status: 404,
+      body: createHtml('<h1>Not found!</h1>')
+    }
+  }
+}
+
+function createHtml(bodyContent) {
+  return `<html>
+    <head><link rel="stylesheet" type="text/css" href="/style.css"></head>
+    <body>${bodyContent}</body>
+  </html>`;
 }
