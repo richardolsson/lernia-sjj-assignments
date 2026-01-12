d460e806d3482ed507ad3e20d6765856f0cea3b6
Richard Olsson <r@richardolsson.se>
2026-01-12 15:48:48 +0100

    Replace custom routing with express.static()

--- classes/a5/a503/examples/ex4-express-fileserver/index.js
+++ classes/a5/a503/examples/ex4-express-fileserver/index.js
@@ -3,6 +3,24 @@ import fs from 'fs/promises';
 
 const app = express();
 
+app.use('/', express.static('./static'));
+
+/*
+app.get('/{*path}', async (req, res) => {
+  const urlPath = req.url;
+  const filePath = './static' + urlPath;
+  const content = await fs.readFile(filePath);
+  const text = content.toString();
+
+  const suffix = urlPath.split('.')[1];
+  console.log(suffix);
+  if (suffix) {
+    res.contentType = suffix;
+  }
+
+  res.send(text);
+});
+
 app.get('/', async (req, res) => {
   const content = await fs.readFile('./static/index.html');
   const html = content.toString();
@@ -21,5 +39,6 @@ app.get('/style.css', async (req, res) => {
   res.setHeader('content-type', 'text/css');
   res.send(css);
 });
+*/
 
 app.listen(5080);
