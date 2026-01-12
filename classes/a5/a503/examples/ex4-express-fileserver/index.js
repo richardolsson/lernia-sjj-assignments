ccde6ff963f701c10f6ba4b784f134461013036b
Richard Olsson <r@richardolsson.se>
2026-01-12 15:34:12 +0100

    Serve both HTML and CSS

--- classes/a5/a503/examples/ex4-express-fileserver/index.js
+++ classes/a5/a503/examples/ex4-express-fileserver/index.js
@@ -3,11 +3,23 @@ import fs from 'fs/promises';
 
 const app = express();
 
+app.get('/', async (req, res) => {
+  const content = await fs.readFile('./static/index.html');
+  const html = content.toString();
+  res.send(html);
+});
+
 app.get('/index.html', async (req, res) => {
   const content = await fs.readFile('./static/index.html');
   const html = content.toString();
-
   res.send(html);
+});
+
+app.get('/style.css', async (req, res) => {
+  const content = await fs.readFile('./static/style.css');
+  const css = content.toString();
+  res.setHeader('content-type', 'text/css');
+  res.send(css);
 });
 
 app.listen(5080);
