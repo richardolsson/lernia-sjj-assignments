68ea04becc3fd88741cae30a1e10d07f142a33ee
Richard Olsson <r@richardolsson.se>
2026-01-14 11:03:23 +0100

    Implement crude homemade templating

--- classes/a5/a504/examples/ex2-express/index.js
+++ classes/a5/a504/examples/ex2-express/index.js
@@ -3,6 +3,32 @@ import fs from 'fs/promises';
 
 const app = express();
 
+async function renderPage(page) {
+  const templateBuf = await fs.readFile('./templates/page.html');
+  const templateText = templateBuf.toString();
+
+  const contentBuf = await fs.readFile(`./content/${page}.html`);
+  const contentText = contentBuf.toString();
+
+  const htmlText = templateText.replace('::page::', contentText);
+
+  return htmlText;
+}
+
+app.get('/', async (req, res) => {
+  const htmlText = await renderPage('index');
+  res.send(htmlText);
+});
+
+app.get('/contact.html', async (req, res) => {
+  const htmlText = await renderPage('contact');
+  res.send(htmlText);
+});
+
+app.get('/about.html', async (req, res) => {
+  const htmlText = await renderPage('about');
+  res.send(htmlText);
+});
 
 app.use('/', express.static('./static'));
 
