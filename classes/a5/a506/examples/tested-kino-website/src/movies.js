e6141a37c0d191d2e9b851e72a62568caa6ced7b
Richard Olsson <r@richardolsson.se>
2026-01-19 11:01:01 +0100

    Flatten movie object from API to simplify templates

--- classes/a5/a506/examples/tested-kino-website/src/movies.js
+++ classes/a5/a506/examples/tested-kino-website/src/movies.js
@@ -5,11 +5,18 @@ const API_BASE = 'https://plankton-app-xhkom.ondigitalocean.app/api';
 export async function loadMovies() {
   const res = await fetch(API_BASE + '/movies');
   const payload = await res.json();
-  return payload.data;
+  return payload.data.map(flattenMovie);
 }
 
 export async function loadMovie(id) {
   const res = await fetch(API_BASE + '/movies/' + id);
   const payload = await res.json();
-  return payload.data;
+  return flattenMovie(payload.data);
+}
+
+function flattenMovie(movie) {
+  return {
+    id: movie.id,
+    ...movie.attributes,
+  };
 }
