39272340f215999eecf4c387f5b759f72d1de929
Richard Olsson <r@richardolsson.se>
2026-01-23 10:42:06 +0100

    Display top 3 challenges on front page

--- livecoding/03-esc-booking/main.js
+++ livecoding/03-esc-booking/main.js
@@ -1,5 +1,13 @@
+import FakeBackend from "./src/js/backend/FakeBackend.js";
+import Top3ChallengeList from "./src/js/challenges/Top3ChallengeList.js";
+
 const navButton = document.querySelector('.header__navButton');
 navButton.addEventListener('click', () => {
   const nav = document.querySelector('.header');
   nav.classList.toggle('header--navOpen');
 });
+
+const backend = new FakeBackend();
+const top3List = new Top3ChallengeList(backend);
+const listElem = await top3List.render();
+document.querySelector('.challenges__headline').after(listElem);
