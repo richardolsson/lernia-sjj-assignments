793e8c59f32beb33cfe2b55d4f4e0e5a56886fbf
Richard Olsson <r@richardolsson.se>
2026-03-20 08:53:31 +0100

    Add examples from b702 class

--- classes/b7/b702/titlecase.js
+++ classes/b7/b702/titlecase.js
@@ -1,2 +1,33 @@
-export default function titlecase(str) {
+export default function titlecase(input) {
+  /*
+  let output = '';
+  for (let i = 0; i < input.length; i++) {
+    const curLetter = input.charAt(i);
+    const prevLetter = output.charAt(i-1);
+    if (prevLetter == ' ' || !prevLetter) {
+      output += curLetter.toUpperCase();
+    } else {
+      output += curLetter.toLowerCase();
+    }
+  }
+
+  return output;
+  */
+
+  const EXCEPTIONS = ['the', 'and', 'of', 'in', 'to'];
+
+  return input.split(' ').map((word, index) => {
+    const isException = EXCEPTIONS.includes(word);
+    const isFirst = index == 0;
+    const isMeaningfulWord = !isException;
+    if (isFirst || isMeaningfulWord) {
+      return capitalize(word);
+    }
+
+    return word;
+  }).join(' ');
+}
+
+function capitalize(word) {
+  return word.charAt(0).toUpperCase() + word.slice(1).toLowerCase();
 }
--- classes/b7/b702/titlecase.test.js
+++ classes/b7/b702/titlecase.test.js
@@ -1,4 +1,38 @@
+import { describe, expect, it } from '@jest/globals';
+
 import titlecase from "./titlecase";
 
-describe('', () => {
+/*
+En funktion som tar en sträng som input.
+Gör om första bokstaven i varje ord så att den i regel är versal.
+Vissa ord ("of", "the", "in", "to", "and") udantas – ska vara gemener.
+Första ordet i meningen inleds dock alltid med stor bokstav.
+Om strängen är tom, så får man tillbaka tom sträng.
+
+  Ex 1: "lord of the rings" -> "Lord of the Rings"
+  Ex 2: "the two towers" -> "The Two Towers"
+  Ex 3: "of mice and men" -> "Of Mice and Men"
+*/
+
+describe('titlecase()', () => {
+  it('returns empty string if argument is empty string', () => {
+    const output = titlecase('');
+    expect(output).toBe('');
+  });
+
+  it('does nothing when first letter is already upper-case', () => {
+    const output = titlecase('Hello');
+    expect(output).toBe('Hello');
+  });
+
+  it('converts "the two towers" to "The Two Towers"', () => {
+    const output = titlecase('the two towers');
+    expect(output).toBe('The Two Towers');
+  });
+
+  // Parameterized tests
+  it.each(['of', 'the', 'in', 'and', 'to'])('treats "%s" as exception', (word) => {
+    const output = titlecase(`fight ${word} power`);
+    expect(output).toBe(`Fight ${word} Power`);
+  });
 });
