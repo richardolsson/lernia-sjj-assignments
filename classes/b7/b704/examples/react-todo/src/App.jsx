909bcc4c09698b809c4810b914d45b926fb49ee7
Richard Olsson <r@richardolsson.se>
2026-03-23 15:50:09 +0100

    Implement state management for todo items

--- classes/b7/b704/examples/react-todo/src/App.jsx
+++ classes/b7/b704/examples/react-todo/src/App.jsx
@@ -1,5 +1,7 @@
+import { useState } from "react";
+
 function App() {
-  const items = [
+  const [items, setItems] = useState([
     {
       label: 'Learn HTML',
       completed: true,
@@ -12,14 +14,16 @@ function App() {
       label: 'Learn React',
       completed: false,
     }
-  ];
+  ]);
+
+  const completedItems = items.filter(item => item.completed);
 
   console.log('render!');
 
   return (
     <main class="app">
       <h1 class="app__title">My ToDo</h1>
-      <small class="app__counter">2 completed</small>
+      <small class="app__counter">{completedItems.length} completed</small>
 
       <form class="taskForm">
         <input class="taskForm__input" name="taskText" type="text" />
@@ -27,14 +31,22 @@ function App() {
         <p class="taskForm__error"></p>
       </form>
       <ul class="taskList">
-        {items.map(item => {
+        {items.map((item, index) => {
           let classes = 'taskList__item';
           if (item.completed) {
             classes += ' taskList__item--completed';
           }
 
           return (
-            <li class={classes}>
+            <li class={classes} onClick={() => {
+              console.log('click!');
+              const oldItems = items;
+              const newItems = oldItems.map((oldItem, oldIndex) => ({
+                label: oldItem.label,
+                completed: oldIndex == index ? !oldItem.completed : oldItem.completed,
+              }))
+              setItems(newItems);
+            }}>
               {item.label}<button class="taskList__itemDeleteButton">🗑</button>
             </li>
           )
