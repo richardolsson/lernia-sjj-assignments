21ffc34d5654506880ebc1235a358d504977ea36
Richard Olsson <r@richardolsson.se>
2026-03-23 15:28:45 +0100

    Add example of React and JSX

--- classes/b7/b704/examples/react-todo/src/App.jsx
+++ classes/b7/b704/examples/react-todo/src/App.jsx
@@ -1,5 +1,32 @@
+import React from 'react';
+
 function App() {
-  return 'hello';
+  /*
+  return (
+    React.createElement('div', {}, [
+      React.createElement('h1', { style: { color: 'red' }}, [
+        'Hello'
+      ]),
+      React.createElement('p', {}, [
+        'This is ',
+        React.createElement('strong', {}, [
+          'React'
+        ])
+      ])
+    ])
+  );
+  */
+
+  return (
+    <div>
+      <h1 style={{color: 'red'}}>
+        Hello
+      </h1>
+      <p>
+        This is <strong>React</strong>
+      </p>
+    </div>
+  )
 }
 
 export default App
