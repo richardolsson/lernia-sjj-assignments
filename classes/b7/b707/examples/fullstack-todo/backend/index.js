4027d63e130178c8c8fc26886d44c0ff047e2313
Richard Olsson <r@richardolsson.se>
2026-03-30 11:32:01 +0200

    Add API route to create new tasks

--- classes/b7/b707/examples/fullstack-todo/backend/index.js
+++ classes/b7/b707/examples/fullstack-todo/backend/index.js
@@ -2,25 +2,36 @@ import express from 'express';
 
 const app = express();
 
+app.use(express.json());
+
+const TASKS = [
+  {
+    label: 'Learn HTML',
+    completed: true,
+  },
+  {
+    label: 'Learn CSS',
+    completed: true,
+  },
+  {
+    label: 'Learn React',
+    completed: true,
+  },
+  {
+    label: 'Learn fullstack',
+    completed: false,
+  },
+];
+
 app.get('/api/tasks', (req, res) => {
-  res.json([
-    {
-      label: 'Learn HTML',
-      completed: true,
-    },
-    {
-      label: 'Learn CSS',
-      completed: true,
-    },
-    {
-      label: 'Learn React',
-      completed: true,
-    },
-    {
-      label: 'Learn fullstack',
-      completed: false,
-    },
-  ]);
+  res.json(TASKS);
+});
+
+app.post('/api/tasks', (req, res) => {
+  const task = req.body;
+  TASKS.push(task);
+
+  res.status(201).json(task);
 });
 
 app.listen(5080);
