d3c1fd57fed0577716854420c54f11d8eac2b85c
Richard Olsson <r@richardolsson.se>
2026-04-23 14:21:09 +0200

    Implement loading of tasks from Postgres database

--- classes/b7/b718/examples/pg-todo/backend/index.js
+++ classes/b7/b718/examples/pg-todo/backend/index.js
@@ -1,12 +1,29 @@
 import fs from 'fs/promises';
 import express from 'express';
 
+import { Pool } from 'pg';
+
+const pool = new Pool({
+  user: 'postgres',
+  password: process.env.POSTGRES_PASSWORD,
+  host: 'dbservice',
+});
+
+await pool.query(`
+  CREATE TABLE IF NOT EXISTS tasks (
+    id SERIAL PRIMARY KEY,
+    label TEXT NOT NULL,
+    completed BOOLEAN NOT NULL
+  )
+`);
+
 const app = express();
 
 app.use(express.json());
 
 app.get('/api/tasks', async (req, res) => {
-  res.json({});
+  const result = await pool.query('SELECT * FROM tasks');
+  res.json(result.rows);
 });
 
 app.post('/api/tasks', async (req, res) => {
