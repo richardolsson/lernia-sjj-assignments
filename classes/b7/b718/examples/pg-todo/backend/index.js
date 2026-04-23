d13279bcd3e85768a0d12211ab42ef464f2913f0
Richard Olsson <r@richardolsson.se>
2026-04-23 14:10:42 +0200

    Remove all traces of MongoDB

--- classes/b7/b718/examples/pg-todo/backend/index.js
+++ classes/b7/b718/examples/pg-todo/backend/index.js
@@ -1,32 +1,16 @@
 import fs from 'fs/promises';
 import express from 'express';
-import mongoose from 'mongoose';
-
-import Task from './src/models/Task.js';
-
-const MONGODB_URL = process.env.MONGODB_URL;
 
 const app = express();
 
 app.use(express.json());
 
 app.get('/api/tasks', async (req, res) => {
-  await mongoose.connect(MONGODB_URL);
-  const tasks = await Task.find();
-  res.json(tasks);
+  res.json({});
 });
 
 app.post('/api/tasks', async (req, res) => {
-  await mongoose.connect(MONGODB_URL);
-
-  const newTask = new Task({
-    label: req.body.label,
-    completed: req.body.completed,
-  });
-
-  await newTask.save();
-
-  res.status(201).json(newTask);
+  res.status(201).json({});
 });
 
 app.get('/', async (req, res) => {
