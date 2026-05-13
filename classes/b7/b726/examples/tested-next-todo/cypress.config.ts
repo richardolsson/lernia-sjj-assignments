d206f0309d1076c269ee04b91974392208ce5f98
Richard Olsson <r@richardolsson.se>
2026-05-13 10:43:05 +0200

    Add cypress boilerplate

---
+++ classes/b7/b726/examples/tested-next-todo/cypress.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from "cypress";
+
+export default defineConfig({
+  allowCypressEnv: false,
+  e2e: {
+    supportFile: false,
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+  },
+});
