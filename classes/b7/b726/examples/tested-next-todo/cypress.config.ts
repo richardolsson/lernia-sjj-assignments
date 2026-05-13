e6c689435a90f0d171b044d08e2720d5289231b9
Richard Olsson <r@richardolsson.se>
2026-05-13 11:24:02 +0200

    Add task to reset database

--- classes/b7/b726/examples/tested-next-todo/cypress.config.ts
+++ classes/b7/b726/examples/tested-next-todo/cypress.config.ts
@@ -1,11 +1,19 @@
+import { TaskModel } from "@/models";
 import { defineConfig } from "cypress";
+import mongoose from "mongoose";
 
 export default defineConfig({
   allowCypressEnv: false,
   e2e: {
     supportFile: false,
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      on('task', {
+        async resetDatabase() {
+          await mongoose.connect('mongodb://127.0.0.1:27017/test');
+          await TaskModel.deleteMany();
+          return null;
+        }
+      });
     },
   },
 });
--- classes/b7/b726/examples/tested-next-todo/cypress/e2e/spec.cy.ts
+++ classes/b7/b726/examples/tested-next-todo/cypress/e2e/spec.cy.ts
@@ -1,5 +1,6 @@
 describe('todo app', () => {
   it('can add task to list', () => {
+    cy.task('resetDatabase');
     cy.visit('http://localhost:3000')
     cy.get('input').focus().type('Learn e2e testing');
     cy.get('[data-testid=taskForm__submitButton]').click();
