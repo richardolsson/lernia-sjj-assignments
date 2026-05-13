ab2dad2babc03f6cffdacbd37318126400c11227
Richard Olsson <r@richardolsson.se>
2026-05-13 11:25:58 +0200

    Add another test and remove resetDatabase to beforeEach()

--- classes/b7/b726/examples/tested-next-todo/cypress/e2e/spec.cy.ts
+++ classes/b7/b726/examples/tested-next-todo/cypress/e2e/spec.cy.ts
@@ -1,6 +1,9 @@
 describe('todo app', () => {
-  it('can add task to list', () => {
+  beforeEach(() => {
     cy.task('resetDatabase');
+  });
+
+  it('can add task to list', () => {
     cy.visit('http://localhost:3000')
     cy.get('input').focus().type('Learn e2e testing');
     cy.get('[data-testid=taskForm__submitButton]').click();
@@ -9,4 +12,10 @@ describe('todo app', () => {
     cy.visit('http://localhost:3000');
     cy.get('li').contains('Learn e2e testing').should('exist');
   })
+
+  it('can add task to list with enter', () => {
+    cy.visit('http://localhost:3000')
+    cy.get('input').focus().type('Learn e2e testing').press('Enter');
+    cy.get('li').contains('Learn e2e testing').should('exist');
+  });
 })
