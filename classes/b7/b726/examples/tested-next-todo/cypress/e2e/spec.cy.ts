24f8c698dedffb3d0cfe1b86a5a0450546a50619
Richard Olsson <r@richardolsson.se>
2026-05-13 10:51:42 +0200

    Add crude test with several problems

--- classes/b7/b726/examples/tested-next-todo/cypress/e2e/spec.cy.ts
+++ classes/b7/b726/examples/tested-next-todo/cypress/e2e/spec.cy.ts
@@ -1,5 +1,8 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
+describe('todo app', () => {
+  it('can add task to list', () => {
+    cy.visit('http://localhost:3000')
+    cy.get('input').focus().type('Learn e2e testing');
+    cy.get('button').contains("OK").click();
+    cy.get('li').contains('Learn e2e testing').should('exist');
   })
 })
