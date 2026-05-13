81a7c584141008ba54fcf7cd906982094a54bd9f
Richard Olsson <r@richardolsson.se>
2026-05-13 11:01:16 +0200

    Use data-testid to harden tests

--- classes/b7/b726/examples/tested-next-todo/cypress/e2e/spec.cy.ts
+++ classes/b7/b726/examples/tested-next-todo/cypress/e2e/spec.cy.ts
@@ -2,7 +2,7 @@ describe('todo app', () => {
   it('can add task to list', () => {
     cy.visit('http://localhost:3000')
     cy.get('input').focus().type('Learn e2e testing');
-    cy.get('button').contains("OK").click();
+    cy.get('[data-testid=taskForm__submitButton]').click();
     cy.get('li').contains('Learn e2e testing').should('exist');
   })
 })
--- classes/b7/b726/examples/tested-next-todo/src/components/TaskForm.tsx
+++ classes/b7/b726/examples/tested-next-todo/src/components/TaskForm.tsx
@@ -21,7 +21,11 @@ const TaskForm: FC<Props> = ({ onCreateItem }) => {
         value={text}
         onChange={(event) => setText(event.target.value)}
       />
-      <button className="taskForm__submitButton" type="submit">OK</button>
+      <button
+        className="taskForm__submitButton"
+        data-testid="taskForm__submitButton"
+        type="submit"
+      >OK</button>
       <p className="taskForm__error"></p>
     </form>
   );
