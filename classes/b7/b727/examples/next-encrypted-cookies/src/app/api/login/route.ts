cd3c9f5871f8f3e74fec7912b509e8b79daa54e3
Richard Olsson <r@richardolsson.se>
2026-05-18 10:59:39 +0200

    Add boilerplate for secure examples (JWT and encryption)

---
+++ classes/b7/b727/examples/next-encrypted-cookies/src/app/api/login/route.ts
@@ -0,0 +1,13 @@
+import { NextRequest, NextResponse } from "next/server";
+
+export async function POST(request: NextRequest) {
+  const payload = await request.json();
+
+  if (payload.username == 'admin' && payload.password == 'password') {
+    const response = new NextResponse();
+    response.cookies.set('username', payload.username);
+    return response;
+  } else {
+    return NextResponse.json({}, { status: 401 });
+  }
+}
---
+++ classes/b7/b727/examples/next-jwt-cookies/src/app/page.tsx
@@ -0,0 +1,11 @@
+import LoginForm from "./components/LoginForm";
+
+export default function Home() {
+  return (
+    <div>
+
+      <h1>Hello</h1>
+      <LoginForm />
+    </div>
+  );
+}
