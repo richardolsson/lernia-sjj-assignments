2312f7ea4e9a4ae46fe4b5153817c5be6b1b0f9e
Richard Olsson <r@richardolsson.se>
2026-05-20 10:59:44 +0200

    Use bcrypt to verify password

--- classes/b7/b728/examples/next-jwt-cookies/src/app/api/login/route.ts
+++ classes/b7/b728/examples/next-jwt-cookies/src/app/api/login/route.ts
@@ -1,22 +1,36 @@
+import bcrypt from 'bcrypt';
 import jsonwebtoken from 'jsonwebtoken';
 import { NextRequest, NextResponse } from "next/server";
+
+// TODO: This should be in a database instead
+const USERS = [
+  {
+    username: 'admin',
+    passwordHash: '$2b$10$Brt5GnlSLVWxQAV3UTVwhOt6hZ7NU7RE2AhmUMUp1UXSEFh/Fvvf.',
+  },
+];
 
 export async function POST(request: NextRequest) {
   const payload = await request.json();
 
-  if (payload.username == 'admin' && payload.password == 'password') {
-    const response = new NextResponse();
-    const sessionData = {
-      username: payload.username,
-      loginTime: new Date().toISOString(),
-    };
+  const user = USERS.find(candidate => candidate.username == payload.username);
+  if (user) {
+    const passwordIsCorrect = await bcrypt.compare(payload.password, user.passwordHash);
 
-    const jwt = jsonwebtoken.sign(sessionData, process.env.JWT_KEY || '');
+    if (passwordIsCorrect) {
+      const response = new NextResponse();
+      const sessionData = {
+        username: payload.username,
+        loginTime: new Date().toISOString(),
+      };
 
-    response.cookies.set('session', jwt);
+      const jwt = jsonwebtoken.sign(sessionData, process.env.JWT_KEY || '');
 
-    return response;
-  } else {
-    return NextResponse.json({}, { status: 401 });
+      response.cookies.set('session', jwt);
+
+      return response;
+    }
   }
+
+  return NextResponse.json({}, { status: 401 });
 }
